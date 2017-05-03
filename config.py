e174f2bdf9d72edfe66221681d41ce090f2908e8
Ilan Filonenko <if56@cornell.edu>
2017-05-03 01:08:32 -0400

    bring back .env

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
   CSRF_ENABLED = True
   CSRF_SESSION_KEY = "secret"
   SECRET_KEY = "not_this"
-  SQLALCHEMY_DATABASE_URI = "postgresql://localhost/mydb"
+  SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class ProductionConfig(Config):
   DEBUG = False
