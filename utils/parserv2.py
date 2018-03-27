2c1ca8b6d1c6d3a52bc23cc68ed77e6dcb665d21
Eric Feng <evf23@cornell.edu>
2018-03-26 20:48:49 -0400

    finalize parser

--- utils/parserv2.py
+++ utils/parserv2.py
@@ -8,29 +8,29 @@ filename = sys.argv[1]
 print "starting parser"
 start_time = int(time.time())
 
-rel_comments = 0
+rel_comments = []
 counter = 0
+
 with open(filename, "r") as file:
-  with open(filename + ".json", 'w') as outfile:
-    print "file loaded!"
-    print "-----------------"
+  print "file loaded!"
+  print "-----------------"
 
-    flag = True
+  flag = True
 
-    for comment in file:
-      counter+=1
-      if flag:
-        print "beginning to read lines..."
-        epoch_time = int(time.time())
+  for comment in file:
+    counter+=1
+    if flag:
+      print "beginning to read lines..."
 
-        flag = False
-      obj = json.loads(comment)
-      if obj["subreddit"].lower() in subreddits:
-        if obj["body"] == "[removed]" or obj["body"] == "[deleted]" or obj["score"] < 0:
-          continue
-        rel_comments+=1
-        outfile.write(json.dumps(obj))
+      flag = False
+    obj = json.loads(comment)
+    if obj["subreddit"].lower() in subreddits:
+      if obj["body"] == "[removed]" or obj["body"] == "[deleted]" or obj["score"] < 0:
+        continue
+      rel_comments.append(json.dumps(obj))
 
+with open(filename + ".json", 'w') as outfile:
+  json.dump(rel_comments, outfile)
 end_time = int(time.time())
 print "Took", (end_time - start_time), "seconds to parse", counter, "comments."
 print "Resulting list has", len(rel_comments), "comments"
