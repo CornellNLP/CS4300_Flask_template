2ecab1a866ff50d1f50f48b72067f75352d0fccb
Eric Feng <evf23@cornell.edu>
2018-03-27 18:10:41 -0400

    update parser

--- utils/parserv2.py
+++ utils/parserv2.py
@@ -25,12 +25,57 @@ with open(filename, "r") as file:
       flag = False
     obj = json.loads(comment)
     if obj["subreddit"].lower() in subreddits:
-      if obj["body"] == "[removed]" or obj["body"] == "[deleted]" or obj["score"] < 0:
-        continue
-      rel_comments.append(json.dumps(obj))
+      # eliminates short replies
+      short_reply = "t1" in obj["parent_id"] and len(obj["body"]) < 30
 
-with open(filename + ".json", 'w') as outfile:
-  json.dump(rel_comments, outfile)
+      # elimintes thanks!
+      is_thanks_reply = "t1" in obj["parent_id"] and "thank" in obj["body"].lower()
+
+      # eliminates removed comments
+      is_deleted = obj["body"] == "[removed]" or obj["body"] == "[deleted]"
+
+      # eliminates poor scoring comments
+      downvoted = obj["score"] < 0
+
+      # eliminates most useless ELI5 comments
+      bad_eli5 = obj["score"] <= 1 and obj["subreddit"].lower() == "explainlikeimfive"
+      bad_comment = short_reply or is_deleted or downvoted
+      if bad_comment:
+        continue
+      rel_comments.append(obj)
+
 end_time = int(time.time())
 print "Took", (end_time - start_time), "seconds to parse", counter, "comments."
-print "Resulting list has", len(rel_comments), "comments"
+print "Initial resulting list has", len(rel_comments), "comments"
+
+max_depth = 4
+rel_tree = {}
+filtered_comments = []
+
+print "Trimming comments past depth", max_depth
+start_time = int(time.time())
+
+# get all top level comments
+for comment in rel_comments:
+  if "t3" in comment["parent_id"]:
+    rel_tree[comment["id"]] = 0
+    rel_comments.remove(comment)
+    filtered_comments.append(comment)
+
+print "finished finding parent comments"
+
+for i in range(max_depth):
+  for comment in rel_comments:
+    if comment["parent_id"] in rel_tree:
+      rel_tree[rel_tree[comment["id"]]] = comment["parent_id"]+1
+      rel_comments.remove(comment)
+      filtered_comments.append(comment)
+  print "finished iteration", i
+
+end_time = int(time.time())
+print "Took", (end_time - start_time), "seconds to filter by depth"
+
+with open(filename + ".json", 'w') as outfile:
+  json.dump(filtered_comments, outfile)
+
+print "Final resulting list has", len(filtered_comments), "comments"
