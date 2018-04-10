25de98893f9d27f06712467dce054ea30083a6ed
joseph-mcallister <joseph.l.mcallister@gmail.com>
2018-04-10 19:17:51 -0400

    add vote call

--- app/irsystem/models/database_helpers.py
+++ app/irsystem/models/database_helpers.py
@@ -11,8 +11,11 @@ def get_donations(politician_name):
 def get_tweets_by_politician(politician_name):
     return db.tweets.find({"PoliticianName":politician_name})
 
+def get_votes_by_politician(politician_name):
+    return db.votes.find({"vote.positions.PoliticianName":politician_name})
+
 # Run once to optimize querying by politician name (turns key into index)
 def create_indexes():
     db.donations.create_index("PoliticianName")
     db.tweets.create_index("PoliticianName")
-    db.votes.create_index("PoliticianName")
+    print(db.votes.create_index("vote.positions.PoliticianName"))
