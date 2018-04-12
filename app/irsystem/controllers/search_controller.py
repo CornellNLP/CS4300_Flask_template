1a8352ba14266f73ecffb4e275d09bb7d531e935
Xinyu Zhao <xinyuzhao23@gmail.com>
2018-04-11 22:28:52 -0400

    parse input movies

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,4 +1,4 @@
-from . import *  
+from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 import json
@@ -9,9 +9,10 @@ net_ids = "Angela Zhang: az337, Chris Fifty: cjf92, Newton Ni: cn279, Erik Chan:
 
 @irsystem.route('/', methods=['GET'])
 def search():
+	output_message = ""
+	data = []
 	movies_json = json.load(open('movies.json'))
 	genres_json = json.load(open('genres.json'))
-
 	movie_list = [movie['title'] for movie in movies_json]
 	genre_list = [genre['name'] for genre in genres_json['genres']]
 
@@ -27,6 +28,14 @@ def search():
 		data = []
 		output_message = ''
 	else:
+		selected_movies = []
+		if similar:
+			names = []
+			if ';' in similar:
+				names = similar.split(";")
+			for n in names:
+				selected_movies.append(n.lower());
+				
 		data = []
 		movie_dict = dict()
 		score_dict = dict()
@@ -55,16 +64,15 @@ def search():
 			data.append(movie_dict[movie_id])
 
 		output_message = "Your search has been processed."
-		# rec0 = movies_json[randint(0, len(movie_list) - 1)]
-		# rec0['similarity'] = 95.6
-		# rec0['poster'] = 'https://image.tmdb.org/t/p/w1280/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg'
-		# rec1 = movies_json[randint(0, len(movie_list) - 1)]
-		# rec1['similarity'] = 87.2
-		# rec1['poster'] = 'https://image.tmdb.org/t/p/w1280/ylXCdC106IKiarftHkcacasaAcb.jpg'
-		# rec2 = movies_json[randint(0, len(movie_list) - 1)]
-		# rec2['similarity'] = 75.6
-		# rec2['poster'] = 'https://image.tmdb.org/t/p/w1280/eKi8dIrr8voobbaGzDpe8w0PVbC.jpg'
+			# rec0 = movies_json[randint(0, len(movie_list) - 1)]
+			# rec0['similarity'] = 95.6
+			# rec0['poster'] = 'https://image.tmdb.org/t/p/w1280/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg'
+			# rec1 = movies_json[randint(0, len(movie_list) - 1)]
+			# rec1['similarity'] = 87.2
+			# rec1['poster'] = 'https://image.tmdb.org/t/p/w1280/ylXCdC106IKiarftHkcacasaAcb.jpg'
+			# rec2 = movies_json[randint(0, len(movie_list) - 1)]
+			# rec2['similarity'] = 75.6
+			# rec2['poster'] = 'https://image.tmdb.org/t/p/w1280/eKi8dIrr8voobbaGzDpe8w0PVbC.jpg'
 
-		# data = [rec0, rec1, rec2]
+			# data = [rec0, rec1, rec2]
 	return render_template('search.html', name=project_name, netids=net_ids, output_message=output_message, data=data, movie_list=movie_list, genre_list=genre_list)
-
