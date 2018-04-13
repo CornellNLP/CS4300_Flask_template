0352bb4279428b2c56935fbbbc6ed8f92289e2de
Eric Feng <evf23@cornell.edu>
2018-04-13 14:13:24 -0400

    add react router stuff

--- app/frontend/src/index.jsx
+++ app/frontend/src/index.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom'
+
 import './css/index.css';
 import Home from './components/Home';
+
+// place a Header component on the line above Switch to get a universal header
+const Root = () => {
+  <BrowserRouter>
+    <Switch>
+      <Route exact path='/' component={Home}/>
+    </Switch>
+  </BrowserRouter>
+}
 
 ReactDOM.render(<Home />, document.getElementById('root'));
