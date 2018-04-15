46ab57cf28ac54e446e88894fedf57a85d560d8c
Monica Ong <myo3@cornell.edu>
2018-04-15 17:20:02 -0400

    Create simple Result component for when get results

---
+++ app/frontend/src/components/Result.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Result extends Component {
+
+  constructor(props){
+    super(props);
+  }
+
+  render() {
+    return (<div><p>{this.props.text}</p></div>);
+  }
