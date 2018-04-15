2c204de1b430ef6ec579d2854ad43fa4eafe7be3
Hyun Kyo Jung <hyunkyojung@dhcp-rhodes-2907.redrover.cornell.edu>
2018-04-15 19:53:55 -0400

    before heroku deployment

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -3,7 +3,7 @@ import numpy as np
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 ####
-from app.irsystem.models.tfidf import *
+from app.irsystem.models.cossim import *
 from app.irsystem.models.book import *
 import json
 import os
@@ -12,11 +12,36 @@ import csv
 project_name = "BookRec"
 net_id = "Hyun Kyo Jung: hj283"
 
+@irsystem.route('/', methods=['POST'])
+def post():
+	db.create_all()
+	print(os.getcwd())
+	with open('book_to_index.json') as book_to_index_dict:
+		d = json.load(book_to_index_dict)
+		for book in d.keys():
+			b = Book(name = book, index = d[book])
+			db.session.add(b)
+		db.session.commit()
+
+	with open('description_cosine_6140.json') as cosine_similarity_matrix:
+		d = json.load(cosine_similarity_matrix)
+		i = 0
+		for k in d.keys():
+			c = 0
+			for col in d[k]:
+				cossim = COSSIM(RowNo = k, ColNo = c, CellValue = col)
+				db.session.add(cossim)
+				c += 1
+			i += 1
+			print('at %s th book' % i)
+		db.session.commit()
+
+	output_message = ''
+	data = []
+	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
 
 @irsystem.route('/', methods=['GET'])
 def search():
-	
-	#############inserting into the database#####################################################
 	# db.create_all()
 	# print(os.getcwd())
 	# with open('book_to_index.json') as book_to_index_dict:
@@ -24,28 +49,32 @@ def search():
 	# 	for book in d.keys():
 	# 		b = Book(name = book, index = d[book])
 	# 		db.session.add(b)
+	# 	db.session.commit()
 
-	# with open('description_cosine_small.csv') as cosine_similarity_matrix_csv:
-	# 	matrix_reader = csv.reader(cosine_similarity_matrix_csv, delimiter = ' ', quotechar='|')
-	# 	r = 0
-	# 	for row in matrix_reader:
+	# with open('description_cosine_6140.json') as cosine_similarity_matrix:
+	# 	d = json.load(cosine_similarity_matrix)
+	# 	i = 0
+	# 	for k in d.keys():
 	# 		c = 0
-	# 		for col in row:
-	# 			tfidf = TFIDF(RowNo = r, ColNo = c, CellValue = col)
+	# 		for col in d[k]:
+	# 			cs = COSSIM(RowNo = k, ColNo = c, CellValue = col)
+	# 			db.session.add(cs)
 	# 			c += 1
-	# 			db.session.add(tfidf)
-	# 		r += 1
-	# db.session.commit()
-	#############################################################################################
+	# 		i += 1
+	# 		print('at %s th book' % i)
+	# 	db.session.commit()
 
 	title_input = request.args.get('title_search')
 	keyword_input = request.args.get('keyword_search')
 
+	book_list = [b.name for b in Book.query.all()]
+	print(len(book_list))
 	#user has not inputted anything yet. 
-	if title_input in ["", None]:	 
-		book_list = [b.name for b in Book.query.all()]
-		print(book_list)
-
+	if title_input not in book_list:	 
+		print("No match!")
+		print(book_list[0])
+		print(len(COSSIM.query.all()))
+		
 	#we got user input. Now, we can find similar books.
 	else : 
 		print("user input is %s" % (title_input)) 
@@ -54,7 +83,7 @@ def search():
 		usr_bk_idx = usr_bk.index
 		usr_bk_name = usr_bk.name
 
-		all_bks = TFIDF.query.filter_by(RowNo = usr_bk_idx).all()
+		all_bks = COSSIM.query.filter_by(RowNo = usr_bk_idx).all()
 
 		cos_sim_list = []
 		for bk in all_bks:
@@ -69,9 +98,9 @@ def search():
 			ith_sim_bk = Book.query.filter_by(index = i).first()
 			topten_bk.append((ith_sim_bk.name, cos_sim_list[i]))
 
-	if not title_input:
+	if title_input not in book_list:
 		data = []
-		output_message = ''
+		output_message = 'No match! Sorry.'
 	else:
 		output_message = "Top ten similar books to : " + title_input
 		data = topten_bk
@@ -82,7 +111,7 @@ def search():
 
 
 
-	##How to empty the database###### 
-	##db.reflect()
-	##db.drop_all()
+	#How to empty the database###### 
+	#db.reflect()
+	#db.drop_all()
 	
