d350016638e4bb9abbbb6fd07af6548bbda4de69
jillwuu <jillwuu@users.noreply.github.com>
2018-04-15 15:45:10 -0400

    search bar

--- app/frontend/src/components/Home.js
+++ app/frontend/src/components/Home.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react';
 
 class Home extends Component {
-  render() {
-    return (
-      <p> test </p>
-    );
-  }
+	constructor(props){
+		super(props);
+		this.state = {value: ''};
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(event){
+		this.setState({value: event.target.value});
+	}
+
+	handleSubmit(event){
+		//putting an alert for now, should instead send api request on submit
+		alert('Your search was: ' + this.state.value);
+		event.preventDefault();
+	}
+
+
+
+	render() {
+	    return (
+	      <form onSubmit = {this.handleSubmit}>
+	      	<label>
+	      		<input className = "searchBar" type="text" value={this.state.value} onChange={this.handleChange} />
+	      		<input type="submit" value="Search"/>
+	      	</label>
+	      	
+	      </form>
+	    );
+	}
 }
 
 export default Home;
