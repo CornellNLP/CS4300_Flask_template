06319e86ba8b144c5ea15e0701bd06e37cf83d93
brodyzt <brodyzt@gmail.com>
2018-04-15 19:17:05 -0400

    working on search

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -2,7 +2,7 @@ from . import *
 from app.irsystem.models.helpers import *
 from app import reddit
 from nltk.tokenize import TreebankWordTokenizer
-from collections import Counter
+from collections import Counter, defaultdict
 from app import app
 import flask, os, pickle, json
 
@@ -20,8 +20,11 @@ def search2():
 	print(query)
 	index = build_index(query)
 	results = index_search(query, index, app.config['idfs'], app.config['doc_norms'])
-	print(results)
-	return str(app.config['index'])
+	test = get_reddit_comment_as_json(results[0])
+	# print(test)
+	# print(results)
+	jsons = [get_reddit_comment_as_json(result) for result in results[:10]]
+	return str(jsons)
 
 def build_index(input_string):
 	tokenizer = TreebankWordTokenizer()
@@ -40,23 +43,24 @@ def build_index(input_string):
 	return index
 
 def get_reddit_comment_as_json(id):
-  """
-  Given a comment id, queries reddit API for the info and
-  returns a json comment containing the body, author,
-  score, upvotes/downvotes, subreddit, its permalink and
-  the number of gilds it has
-  """
-  comment = reddit.comment(id=id)
-  comment_json = {}
-  comment_json["body"] = comment.body
-  comment_json["author"] = comment.author
-  comment_json["score"] = comment.score
-  comment_json["ups"] = comment.ups
-  comment_json["downs"] = comment.downs
-  comment_json["subreddit"] = comment.subreddit_name_prefixed
-  comment_json["permalink"] = comment.permalink
-  comment_json["gilded"] = comment.gilded
-  return json.dumps(comment_json)
+	print("ID: " + str(id))
+	"""
+	Given a comment id, queries reddit API for the info and
+	returns a json comment containing the body, author,
+	score, upvotes/downvotes, subreddit, its permalink and
+	the number of gilds it has
+	"""
+	comment = reddit.comment(id=id)
+	comment_json = {}
+	comment_json["body"] = str(comment.body)
+	# comment_json["author"] = comment.author
+	# comment_json["score"] = comment.score
+	# comment_json["ups"] = comment.ups
+	# comment_json["downs"] = comment.downs
+	# comment_json["subreddit"] = comment.subreddit_name_prefixed
+	# comment_json["permalink"] = comment.permalink
+	# comment_json["gilded"] = comment.gilded
+	return comment_json
 
 def index_search(query, index, idf, doc_norms):
     """ Search the collection of documents for the given query
@@ -85,19 +89,20 @@ def index_search(query, index, idf, doc_norms):
 
     tokenizer = TreebankWordTokenizer()
     tokens = tokenizer.tokenize(query.lower())
-    scores = np.zeros(len(doc_norms))
+    scores = defaultdict(int)
     counts = Counter(tokens)
     query_norm = np.linalg.norm(
-        [val * idf[token] for (token, val) in counts.items() if token in idf])
+		[val * idf[token] for (token, val) in counts.items() if token in idf])
 
     for (token, query_count) in counts.items():
         if token in idf:
-            for (doc_id, doc_count) in index[token]:
-                scores[doc_id] += doc_count * \
-                    (idf[token] ** 2) * query_count / \
-                    (doc_norms[doc_id] * query_norm + 1)
+			print(list(index[token])[0])
+			for (doc_id, doc_count) in index[token].items():
+				scores[doc_id] += doc_count * \
+					(idf[token] ** 2) * query_count / \
+					(doc_norms[doc_id] * query_norm + 1)
 
     indexed_list = [(val, i) for i, val in enumerate(scores)]
-    output = sorted(indexed_list, key=lambda x: x[0], reverse=True)
+    output = sorted(indexed_list, key=lambda x: x[1], reverse=True)
 
-    return output
+    return [str(comment[0]) for comment in output]
