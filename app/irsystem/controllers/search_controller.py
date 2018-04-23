09b892b9ab76e7adfbca991bd30f3fd70c91b334
Hyun Kyo Jung <hyunkyojung@dhcp-rhodes-2265.redrover.cornell.edu>
2018-04-23 00:30:16 -0400

    hashing working version

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -9,8 +9,8 @@ from sklearn.preprocessing import normalize
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 ####
-from app.irsystem.models.word import *
-from app.irsystem.models.book import *
+from app.irsystem.models.words import *
+from app.irsystem.models.books import *
 import json
 import os
 import csv
@@ -18,61 +18,60 @@ import csv
 project_name = "BookRec"
 net_id = "Hyun Kyo Jung: hj283"
 
-def closest_books_to_word(word_in, word_to_index, index_to_book, words_compressed, docs_compressed, k = 15):
-    if word_in not in word_to_index: return ["Not in vocab."] 
-    sims = docs_compressed.dot(words_compressed[int(word_to_index[word_in]),:])
-    asort = np.argsort(-sims)[:k+1]
-    return [(index_to_book[str(i)],sims[i]/sims[asort[0]]) for i in asort[1:]]
-
-def create_books_to_wordcloud(title_in, index_to_word, book_to_index, words_compressed , docs_compressed, k = 5):
-    if str(title_in) not in book_to_index: return ["Not in vocab."]
-    sims = words_compressed.dot(docs_compressed[int(book_to_index[title_in]),:])
-    asort = np.argsort(-sims)[:k+1]
-    return [(index_to_word[str(i)],sims[i]/sims[asort[0]])for i in asort[1:]]
-
-# Ability to add multiple words 
-def closest_books_to_many_words(word_in, word_to_index, index_to_book, words_compressed , docs_compressed, k = 15):
-    msg = ""
-    sims = np.zeros(docs_compressed.shape[0])
-    count = 0
-    for w in word_in:
-        if w not in word_to_index: 
-        	msg = w + "is not in the vocab"
-        else:
-            count += 1
-            sims += docs_compressed.dot(words_compressed[int(word_to_index[w]),:])
-    if count == 0 : return ["None of the words are in our vocab"]
-    #sims=sims/count
-    asort = np.argsort(-sims)[:k+1] 
-    lst = [(index_to_book[str(i)],sims[i]/sims[asort[0]]) for i in asort[1:]]
-    lst.append(msg)
-    return lst
-
-def db_word_to_closest_books(word, k = 15):
-	np_word = np.fromstring(word.vector, sep= ', ')
+def db_word_to_closest_books(word, ith, k = 15):
+	avg_word = np.zeros(200)
+	for w, i in zip(word, ith):
+		np_word = np.fromstring(w.vectors, sep= ', ')
+		td_np_word = np.reshape(np_word, (100,200))
+		np_word = td_np_word[i]
+		avg_word += np_word
+	avg_word /= len(word)
 	print('before query')
-	query_result = Book.query.all()
+	query_result = Books.query.all()
 	print('after query')
-	dot_products = np.zeros(len(query_result))
+	dot_products = np.zeros(len(query_result*100))
 	print('before processing')
+	count = 0
 	for book in query_result:
-		np_book = np.fromstring(book.vector, sep = ', ')
-		dot_prod = np_word.dot(np_book)
-		dot_products[book.index] = dot_prod
+		np_book = np.fromstring(book.vectors, sep = ', ')
+		num_books = len(np_book) / 200
+		td_np_book = np.reshape(np_book, (num_books, 200))
+		dot_prod = np.dot(td_np_book, avg_word)
+		print(count)
+		count+=1
+		for i in range(num_books):
+			dot_products[book.start_index + i] = dot_prod[i]
 	print('after processing')
 	asort = np.argsort(-dot_products)[:k+1]
-	return [(Book.query.filter_by(index = i).first().name, dot_products[i]/dot_products[asort[0]]) for i in asort[1:]]
 
-def db_book_to_closest_words(book, k = 5):
-	np_book = np.fromstring(book.vector, sep= ', ')
-	query_result = Word.query.all()
-	dot_products = np.zeros(len(query_result))
+	top_k_books = []
+	for i in asort[1:]:
+		near_names = Books.query.filter_by(start_index = i/100*100).first().names
+		name = near_names.split('***')[i % 100]
+		top_k_books.append((name, dot_products[i]/dot_products[asort[0]]))
+	return top_k_books
+
+def db_book_to_closest_words(book, ith, k = 5):
+	np_book = np.fromstring(book.vectors, sep= ', ')
+	td_np_book = np.reshape(np_book, (100,200))
+	np_book = td_np_book[ith]
+	query_result = Words.query.all()
+	dot_products = np.zeros(len(query_result*100))
 	for word in query_result:
-		np_word = np.fromstring(word.vector, sep = ', ')
-		dot_prod = np_book.dot(np_word)
-		dot_products[word.index] = dot_prod
+		np_word = np.fromstring(word.vectors, sep = ', ')
+		num_words = len(np_word) / 200
+		td_np_word = np.reshape(np_word, (num_words, 200))
+		dot_prod = np.dot(td_np_word, np_book)
+		for i in range(num_words):
+			dot_products[word.start_index + i] = dot_prod[i]
 	asort = np.argsort(-dot_products)[:k+1]
-	return [(Word.query.filter_by(index = i).first().name, dot_products[i]/dot_products[asort[0]]) for i in asort[1:]]
+
+	top_k_words = []
+	for i in asort[1:]:
+		near_names = Words.query.filter_by(start_index = i/100*100).first().names
+		name = near_names.split('***')[i % 100]
+		top_k_words.append((name, dot_products[i]/dot_products[asort[0]]))
+	return top_k_words
 
 #Empties out all tables within the postgresql database
 def empty_db():
@@ -85,7 +84,7 @@ def create_tables():
 	db.create_all()
 
 #Create a book instance
-def put_books_in_db():
+def put_books_in_db(hash_factor = 100):
 	#load the files
 	docs_compressed = pickle.load(open("docs.pkl", "rb"))
 	index_to_book = json.load(open("index_to_book.json"))
@@ -93,152 +92,119 @@ def put_books_in_db():
 	index_to_word = json.load(open("index_to_word.json"))
 	print('files all opened!')
 
+	num_doc = len(docs_compressed)
 	row_i = 0
-	for book in docs_compressed:
-		v = str(book.tolist())[1:-1] #this will be in the format of '-0.8465, -0.28475, 0.3731, ...'
-		b = Book(index = row_i, name = index_to_book[str(row_i)], vector = v)
+	while row_i < num_doc:
+		i = 0
+		hundred_vectors = ''
+		hundred_names   = ''
+		while row_i + i < num_doc and i < hash_factor:
+			if i == 0:
+				hundred_vectors += str(docs_compressed[row_i + i].tolist())[1:-1]
+				hundred_names += index_to_book[str(row_i + i)]
+			else:
+				hundred_vectors = hundred_vectors + ', ' + str(docs_compressed[row_i + i].tolist())[1:-1]
+				hundred_names = hundred_names + '***' + index_to_book[str(row_i + i)]
+			i+=1
+		b = Books(start_index = row_i, names = hundred_names, vectors = hundred_vectors)
 		db.session.add(b)
-		row_i += 1
-	print('done with books')
+		row_i += i
+	print('done with books!')
+	print('last row i was %s' % str(row_i-1))
 
+	num_word = len(words_compressed)
 	row_i = 0
-	for word in words_compressed:
-		v = str(word.tolist())[1:-1] #this will be in the format of '-0.8465, -0.28475, 0.3731, ...'
-		w = Word(index = row_i, name = index_to_word[str(row_i)], vector = v)
+	while row_i < num_word:
+		i = 0
+		hundred_vectors = ''
+		hundred_names   = ''
+		while row_i + i < num_word and i < hash_factor:
+			if i == 0:
+				hundred_vectors += str(words_compressed[row_i + i].tolist())[1:-1]
+				hundred_names += index_to_word[str(row_i + i)]
+			else:
+				hundred_vectors = hundred_vectors + ', ' + str(words_compressed[row_i + i].tolist())[1:-1]
+				hundred_names = hundred_names + '***' + index_to_word[str(row_i + i)]
+			i+=1
+		w = Words(start_index = row_i, names = hundred_names, vectors = hundred_vectors)
 		db.session.add(w)
-		row_i += 1
-	print('done with words')
+		row_i += i
+	print('done with words!')
 
 	db.session.commit()
 	print('commited!')
 
-
+# @irsystem.route('/', methods=['GET'])
+# def search():
+# 	empty_db()
+# 	word_cloud_message = ''
+# 	top_books_message = ''
+# 	word_cloud = ['successfully deleted']
+# 	top_books = ['successfully deleted']
+# 	return render_template('search.html', name=project_name, netid=net_id, word_cloud_message=word_cloud_message, top_books_message=top_books_message, word_cloud=word_cloud, top_books = top_books)
 
 # @irsystem.route('/', methods=['GET'])
 # def search():
+# 	empty_db()
 # 	create_tables()
 # 	put_books_in_db()
-
+# 	word_cloud_message = ''
+# 	top_books_message = ''
+# 	word_cloud = ['successfully added']
+# 	top_books = ['successfully added']
+# 	return render_template('search.html', name=project_name, netid=net_id, word_cloud_message=word_cloud_message, top_books_message=top_books_message, word_cloud=word_cloud, top_books = top_books)
 
 @irsystem.route('/', methods=['GET'])
 def search():
 	title_input = request.args.get('title_search')
 	keyword_input = request.args.get('keyword_search')
 
+	book_to_index = json.load(open("book_to_index.json"))
+	word_to_index = json.load(open("word_to_index.json"))
+
 	if title_input == None and keyword_input == None:
-		print('hey')
 		word_cloud_message = ''
 		top_books_message =  ''
 		word_cloud = []
 		top_books = []
 
 	elif keyword_input is not None: 
-		print('before query')
-		w = Word.query.filter_by(name = keyword_input).first()
-		print('after query')
 		word_cloud_message = ''
 		word_cloud = []
-		if w is None: 
-			print('hey?')
+		top_books = []
+		keywords = keyword_input.split(' ')
+		rel_keywords = []
+		for keyword in keywords:
+			if keyword not in word_to_index.keys():
+				top_books.append(keyword + " is not in our database.")
+			else:
+				rel_keywords.append(keyword)
+		if len(top_books) == len(keywords): 
 			top_books_message = ''
-			top_books = ["The keyword is not in our database."]
 		else:
 			top_books_message = "Top 15 books for the keyword are:"
-			print('before function call')
-			top_books = db_word_to_closest_books(w)
-			print('after function call')
+			word_list = []
+			ith_list = []
+			print(keyword_input)
+			for keyword in rel_keywords:
+				i = word_to_index[keyword]
+				w = Words.query.filter_by(start_index = int(i)/100*100).first()
+				word_list.append(w)
+				ith_list.append(int(i)%100) 
+			for close_book in db_word_to_closest_books(word_list, ith_list):
+				top_books.append(close_book)
 
 	else:
-		b = Book.query.filter_by(name = title_input).first()
 		top_books_message = ""
 		top_books = []
-		if b is None:
+
+		if title_input not in book_to_index.keys():
 			word_cloud_message = ''
 			word_cloud = ['The book is not in our database.']
 		else:
+			i = book_to_index[title_input]
+			b = Books.query.filter_by(start_index = int(i)/100*100).first()
 			word_cloud_message = 'Word cloud is: '	
-			word_cloud = db_book_to_closest_words(b)
+			word_cloud = db_book_to_closest_words(b, int(i) % 100)
 
 	return render_template('search.html', name=project_name, netid=net_id, word_cloud_message=word_cloud_message, top_books_message=top_books_message, word_cloud=word_cloud, top_books = top_books)
-
-
-
-# TODO : need to normalize the docs_compressed 
-# @irsystem.route('/', methods=['GET'])
-# def search():
-# 	###open the files
-# 	empty_db()
-# 	index_to_book = json.load(open("index_to_book.json"))
-# 	index_to_word = json.load(open("index_to_word.json"))	
-
-# 	word_to_index = {value : key for key , value in index_to_word.items()}
-# 	book_to_index = {value : key for key , value in index_to_book.items()}
-# 	words_compressed = pickle.load(open("words.pkl", "rb"))
-# 	docs_compressed = pickle.load(open("docs.pkl", "rb"))
-
-
-# 	title_input = request.args.get('title_search')
-# 	keyword_input = request.args.get('keyword_search')
-
-	
-# 	#ALWAYS NEED THESE
-# 	#word_cloud_message =
-# 	#top_books_message = 
-# 	#word_cloud =
-# 	#top_books = 
-
-# 	#initial
-# 	if title_input == None and keyword_input == None:
-# 		word_cloud_message = ''
-# 		top_books_message =  ''
-# 		word_cloud = []
-# 		top_books = []
-
-
-# 	elif keyword_input is not None: 
-# 		word_cloud_message = ''
-# 		top_books_message = "Top 15 books for the keyword are:"
-# 		word_cloud = []
-# 		lst = keyword_input.split(" ")
-
-# 		top_books = closest_books_to_many_words(lst, word_to_index, index_to_book,words_compressed, docs_compressed)
-
-# 	else:
-# 		word_cloud_message = 'Word cloud is: '
-# 		top_books_message = ""
-# 		word_cloud = create_books_to_wordcloud(title_input, index_to_word, book_to_index, words_compressed , docs_compressed)
-# 		top_books = []
-
-# 	return render_template('search.html', name=project_name, netid=net_id, word_cloud_message=word_cloud_message, top_books_message=top_books_message, word_cloud=word_cloud, top_books = top_books)
-
-  
-	#############inserting into the database#####################################################
-	# db.create_all()
-	# print(os.getcwd())
-	# with open('book_to_index.json') as book_to_index_dict:
-	# 	d = json.load(book_to_index_dict)
-	# 	for book in d.keys():
-	# 		b = Book(name = book, index = d[book])
-	# 		db.session.add(b)
-	# 	db.session.commit()
-
-	# with open('description_cosine_small.csv') as cosine_similarity_matrix_csv:
-	# 	matrix_reader = csv.reader(cosine_similarity_matrix_csv, delimiter = ' ', quotechar='|')
-	# 	r = 0
-	# 	for row in matrix_reader:
-	# 		c = 0
-	# 		for col in row:
-	# 			tfidf = TFIDF(RowNo = r, ColNo = c, CellValue = col)
-	# 			c += 1
-	# 			db.session.add(tfidf)
-	# 		r += 1
-	# db.session.commit()
-	#############################################################################################
-
-
-
-	##How to empty the database###### 
-	##db.reflect()
-	##db.drop_all()
-	
-
