643b60c35dcd421b8b9952b5cee3bd85313abb3d
Eric Feng <evf23@cornell.edu>
2018-04-23 12:40:56 -0400

    weigh based on part of speech

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -13,182 +13,199 @@ lemmatiser = WordNetLemmatizer()
 
 @app.route('/', methods=['GET'])
 def render_homepage():
-	print("loading homepage")
-	# print(url_for('/'))
-	return render_template('index.html')
+  print("loading homepage")
+  # print(url_for('/'))
+  return render_template('index.html')
 
 
 @app.route('/search', methods=['GET'])
 def search2():
-	print('searching:')
-	query = str(request.args.get('query'))
+  print('searching:')
+  query = str(request.args.get('query'))
 
-	# irrelevant_tokens = ['i','want','to','learn','how']
-	irrelevant_tokens = []
+  # irrelevant_tokens = ['i','want','to','learn','how']
+  irrelevant_tokens = []
 
-	tokenizer = TreebankWordTokenizer()
-	tokens = [token for token in tokenizer.tokenize(query.lower()) if token not in irrelevant_tokens]
-	tokens = expand_query(tokens)
+  tokenizer = TreebankWordTokenizer()
+  tokens = [token for token in tokenizer.tokenize(query.lower()) if token not in irrelevant_tokens]
+  tokens = add_stemmed_words(tokens)
 
-	index, tokens = build_index(tokens)
-	results = index_search(tokens, index, app.config['idfs'], app.config['doc_norms'])
+  index, tokens = build_index(tokens)
+  results = index_search(tokens, index, app.config['idfs'], app.config['doc_norms'])
 
-	return json.dumps(results)
+  return json.dumps(results)
 
 def get_wordnet_pos(treebank_tag):
     if treebank_tag.startswith('J'):
-			return wordnet.ADJ
+      return wordnet.ADJ
     elif treebank_tag.startswith('V'):
-			return wordnet.VERB
+      return wordnet.VERB
     elif treebank_tag.startswith('N'):
-			return wordnet.NOUN
+      return wordnet.NOUN
     elif treebank_tag.startswith('R'):
-			return wordnet.ADV
+      return wordnet.ADV
     else:
-			return ''
+      return ''
 
-def get_synonyms(token, treebank_tag):
-  synonyms = set([])
-  for syn in wordnet.synsets(token):
-    for lem in syn.lemmas():
-      synonyms.add(lem.name())
-  return list(synonyms)
+def add_stemmed_words(query_tokens):
+  # builds a list of tokens that are the stemmed versions of the word
+  # see marcobonzanini.com/2015/01/26/stemming-lemmatisation-and-pos-tagging-with-python-and-nltk/
+  addtl_tokens = set([])
+  tokens_pos = pos_tag(query_tokens)
+  for (token, pos_treebank) in tokens_pos:
+    pos = get_wordnet_pos(pos_treebank)
+    if pos == '':
+      continue
+    addtl_tokens.add(lemmatiser.lemmatize(token, pos=pos))
 
-def expand_query(query_tokens):
-	# builds a list of tokens that are the stemmed versions of the word
-	# see marcobonzanini.com/2015/01/26/stemming-lemmatisation-and-pos-tagging-with-python-and-nltk/
-	tokens_pos = pos_tag(query_tokens)
-	addtl_tokens = set([])
-	for (token, pos_treebank) in tokens_pos:
-		pos = get_wordnet_pos(pos_treebank)
-		if pos == '':
-			continue
-		addtl_tokens.add(lemmatiser.lemmatize(token, pos=pos))
-
-		# expand query with synonym
-		addtl_tokens.union(get_synonyms(token, pos_treebank))
-	print addtl_tokens
-	# LMFAO need to do this to ensure no overlap between addtl_tokens and query_tokens
-	return list(set(query_tokens).union(addtl_tokens))
+  # LMFAO need to do this to ensure no overlap between addtl_tokens and query_tokens
+  return list(set(query_tokens).union(addtl_tokens))
 
 def build_index(query_tokens):
-	# builds a query-specfic inverted index by loading the words
-	# in the query that are also in the inverted index
+  # builds a query-specfic inverted index by loading the words
+  # in the query that are also in the inverted index
 
-	tokens = query_tokens
-	print("got index tokens:" + str(tokens))
-	index = dict()
-	for token in tokens:
-		# for word in list(filter(lambda x: x.startswith(token), app.config["valid_words"])):
-		if token in app.config["valid_words"]:
-			f = open(os.getcwd() + "/app/utils/data/" + token + ".pkl","rb")
-			d = pickle.load(f)
-			index[token] = d
-			# if word not in tokens:
-			# 	tokens.append(word)
-	return index, tokens
+  tokens = query_tokens
+  print tokens
+  print("got index tokens:" + str(tokens))
+  index = dict()
+  for token in tokens:
+    if str(token) in app.config["valid_words"]:
+      f = open(os.getcwd() + "/app/utils/data/" + token + ".pkl","rb")
+      d = pickle.load(f)
+      index[token] = d
+    else:
+      index[token] = {}
+  return index, tokens
 
 def index_search(query_tokens, index, idf, doc_norms):
-	""" Search the collection of documents for the given query
+  """ Search the collection of documents for the given query
 
-		Arguments
-		=========
+  Arguments
+  =========
 
-		query: string,
-			The query we are looking for.
+  query: string,
+    The query we are looking for.
 
-		index: an inverted index as above
+  index: an inverted index as above
 
-		idf: idf values precomputed as above
+  idf: idf values precomputed as above
 
-		doc_norms: document norms as computed above
+  doc_norms: document norms as computed above
 
-		Returns
-		=======
+  Returns
+  =======
 
-		results, list of tuples (score, doc_id)
-			Sorted list of results such that the first element has
-			the highest score, and `doc_id` points to the document
-			with the highest score.
+  results, list of tuples (score, doc_id)
+    Sorted list of results such that the first element has
+    the highest score, and `doc_id` points to the document
+    with the highest score.
 
-		"""
-	score_norm = { "iwanttolearn" : 1, "explainlikeimfive" : 0 } # dont consider ELI5 anymore
-	tokens = query_tokens
-	print("Got tokens: " + str(tokens))
+  """
+  score_norm = { "iwanttolearn" : 1, "explainlikeimfive" : 0 } # dont consider ELI5 anymore
+  tokens = query_tokens
 
-	# regular cos-sim without doc_normalization
-	scores = defaultdict(int)
-	counts = Counter(tokens)
-	query_norm = np.linalg.norm(
-		[val * idf[token] for (token, val) in counts.items() if token in idf])
+  tokens_pos = pos_tag(tokens)
+  word_weights = {}
 
-	for (token, query_count) in counts.items():
-		if token in idf and token in index:
-			for (doc_id, doc_count) in index[token].items():
-				scores[doc_id] += doc_count * \
-					(idf[token] ** 2) * query_count / \
-					(query_norm + 1)
+  # nouns are important, want to have them in the response
+  nouns = set([])
 
-	# will map comment ids to the comment dicts
-	id_to_comment = {}
+  # weigh adj/adverbs lower then verbs, which are weighed lower than nouns
+  for (token, treebank_tag) in tokens_pos:
+    if treebank_tag.startswith('J') or treebank_tag.startswith('R'):
+      word_weights[token] = 1
+    elif treebank_tag.startswith('V'):
+      word_weights[token] = 5
+    elif treebank_tag.startswith('N'):
+      word_weights[token] = 10
+      nouns.add(token)
+    else:
+      word_weights[token] = 0
 
-	# get all comments from DB from a list of keys
-	comment_objs = Comment.query.filter(Comment.comment_id.in_(scores.keys())).all()
+  print("Got tokens: " + str(tokens))
 
-	# create the mapping
-	for comment_obj in comment_objs:
-		comment_obj = cvt_Comment_to_dict(comment_obj)
-		id_to_comment[comment_obj["id"]] = comment_obj
+  # regular cos-sim without doc_normalization
+  scores = defaultdict(int)
+  counts = Counter(tokens)
+  query_norm = np.linalg.norm(
+    [val * idf[token] for (token, val) in counts.items() if token in idf])
 
-	# factor in comment scores in the scoring
-	for doc_id in scores:
-		# if comment_id is not in DB for some reason, get rid of it
-		if doc_id not in id_to_comment:
-			scores[doc_id] = -1
-			continue
+  for (token, query_count) in counts.items():
+    if token in idf and token in index:
+      for (doc_id, doc_count) in index[token].items():
+        scores[doc_id] += doc_count * \
+          (idf[token] ** 2) * query_count * word_weights[token] / \
+          (query_norm + 1)
 
-		comment = id_to_comment[doc_id]
+  #significantly reduce scores of comments without nouns in it
+  noun_docs = set([])
+  for token in nouns:
+    noun_docs.update(set(index[token].keys()))
+  for doc_id in scores.keys():
+    if doc_id not in noun_docs:
+      scores[doc_id] *= 0.1
 
-		subreddit = comment["subreddit"].lower()
-		scores[doc_id] *= comment["score"] * score_norm[subreddit]
-		scores[doc_id] += comment["ups"] * score_norm[subreddit]
-		scores[doc_id] -= comment["downs"] * score_norm[subreddit]
+  # will map comment ids to the comment dicts
+  id_to_comment = {}
 
-	# get rid of all doc ids not in the DB
-	filtered_scores = {}
-	for doc_id in scores:
-		if (scores[doc_id] > 0):
-			filtered_scores[doc_id] = scores[doc_id]
+  # get all comments from DB from a list of keys
+  comment_objs = Comment.query.filter(Comment.comment_id.in_(scores.keys())).all()
 
-	output = sorted(filtered_scores.items(), key=lambda x: x[1], reverse=True)[:20]
+  # create the mapping
+  for comment_obj in comment_objs:
+    comment_obj = cvt_Comment_to_dict(comment_obj)
+    id_to_comment[comment_obj["id"]] = comment_obj
 
-	return [id_to_comment[str(comment[0])] for comment in output]
+  # factor in comment scores in the scoring
+  for doc_id in scores:
+    # if comment_id is not in DB for some reason, get rid of it
+    if doc_id not in id_to_comment:
+      scores[doc_id] = -1
+      continue
+
+    comment = id_to_comment[doc_id]
+
+    subreddit = comment["subreddit"].lower()
+    scores[doc_id] *= comment["score"] * score_norm[subreddit]
+    scores[doc_id] += comment["ups"] * score_norm[subreddit]
+    scores[doc_id] -= comment["downs"] * score_norm[subreddit]
+
+  # get rid of all doc ids not in the DB
+  filtered_scores = {}
+  for doc_id in scores:
+    if (scores[doc_id] > 0):
+      filtered_scores[doc_id] = scores[doc_id]
+
+  output = sorted(filtered_scores.items(), key=lambda x: x[1], reverse=True)[:20]
+
+  return [id_to_comment[str(comment[0])] for comment in output]
 
 ################################ HELPER FUNCTIONS ##################################
 
 def get_reddit_comment_as_dict(id):
-	print("ID: " + str(id))
-	"""
-	Given a comment id, queries reddit API for the info and
-	returns a json comment containing the body, author,
-	score, upvotes/downvotes, subreddit, its permalink and
-	the number of gilds it has
-	"""
-	comment = Comment.query.filter_by(comment_id=id).first()
-	if comment is None:
-		return None
-	return cvt_Comment_to_dict(comment)
+  print("ID: " + str(id))
+  """
+  Given a comment id, queries reddit API for the info and
+  returns a json comment containing the body, author,
+  score, upvotes/downvotes, subreddit, its permalink and
+  the number of gilds it has
+  """
+  comment = Comment.query.filter_by(comment_id=id).first()
+  if comment is None:
+    return None
+  return cvt_Comment_to_dict(comment)
 
 def cvt_Comment_to_dict(comment):
-	comment_dict = {}
-	comment_dict["id"] = comment.comment_id
-	comment_dict["body"] = comment.body
-	comment_dict["author"] = comment.author
-	comment_dict["score"] = comment.score
-	comment_dict["ups"] = comment.upvotes
-	comment_dict["downs"] = comment.downvotes
-	comment_dict["subreddit"] = comment.subreddit
-	comment_dict["link_id"] = "https://www.reddit.com/" + comment.link_id[3:]
-	comment_dict["permalink"] = "http://www.reddit.com/comments/" + comment.link_id[3:] + "/_/" + comment.comment_id
-	comment_dict["gilded"] = comment.gilded
-	return comment_dict
+  comment_dict = {}
+  comment_dict["id"] = comment.comment_id
+  comment_dict["body"] = comment.body
+  comment_dict["author"] = comment.author
+  comment_dict["score"] = comment.score
+  comment_dict["ups"] = comment.upvotes
+  comment_dict["downs"] = comment.downvotes
+  comment_dict["subreddit"] = comment.subreddit
+  comment_dict["link_id"] = "https://www.reddit.com/" + comment.link_id[3:]
+  comment_dict["permalink"] = "http://www.reddit.com/comments/" + comment.link_id[3:] + "/_/" + comment.comment_id
+  comment_dict["gilded"] = comment.gilded
+  return comment_dict
