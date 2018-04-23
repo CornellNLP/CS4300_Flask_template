fd31d2255ea64e6ce73d188205f55b18a7e7cd7b
Eric Feng <evf23@cornell.edu>
2018-04-23 00:56:18 -0400

    permalink, thread working again, link to author, and fix footer, stemming

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -2,9 +2,14 @@ from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.comment import Comment
 from nltk.tokenize import TreebankWordTokenizer
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
+from nltk import pos_tag
 from collections import Counter, defaultdict
 from app import app
 import flask, os, pickle, json
+
+lemmatiser = WordNetLemmatizer()
 
 @app.route('/', methods=['GET'])
 def render_homepage():
@@ -16,67 +21,59 @@ def render_homepage():
 @app.route('/search', methods=['GET'])
 def search2():
 	print('searching:')
-	# print(app.config['tf_idfs'])
 	query = str(request.args.get('query'))
-	# print(query)
 
+	# irrelevant_tokens = ['i','want','to','learn','how']
+	irrelevant_tokens = []
 
-	irrelevant_tokens = ['i','want','to','lean','how']
 	tokenizer = TreebankWordTokenizer()
 	tokens = [token for token in tokenizer.tokenize(query.lower()) if token not in irrelevant_tokens]
-	# print(query)
-	# print(tokens)
+	tokens = stem_tokens(tokens)
 
 	index = build_index(tokens)
 	results = index_search(tokens, index, app.config['idfs'], app.config['doc_norms'])
-	# test = get_reddit_comment_as_dict(results[0])
-	# print(test)
-	# print(results)
 
 	return json.dumps(results)
 
+def get_wordnet_pos(treebank_tag):
+
+    if treebank_tag.startswith('J'):
+        return wordnet.ADJ
+    elif treebank_tag.startswith('V'):
+        return wordnet.VERB
+    elif treebank_tag.startswith('N'):
+        return wordnet.NOUN
+    elif treebank_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return ''
+
+def stem_tokens(query_tokens):
+	# builds a list of tokens that are the stemmed versions of the word
+	# see marcobonzanini.com/2015/01/26/stemming-lemmatisation-and-pos-tagging-with-python-and-nltk/
+	tokens_pos = pos_tag(query_tokens)
+	addtl_tokens = []
+	for (token, pos) in tokens_pos:
+		pos = get_wordnet_pos(pos)
+		if pos == '':
+			continue
+		addtl_tokens.append(lemmatiser.lemmatize(token, pos=pos))
+	print addtl_tokens
+	return query_tokens + addtl_tokens
+
 def build_index(query_tokens):
+	# builds a query-specfic inverted index by loading the words
+	# in the query that are also in the inverted index
+
 	tokens = query_tokens
 	print("got index tokens:" + str(tokens))
 	index = dict()
 	for token in tokens:
 		if token in app.config["valid_words"]:
-			# print(filename)
-				#print filename
 			f = open(os.getcwd() + "/app/utils/data/" + token + ".pkl","rb")
-			# print(f)
 			d = pickle.load(f)
-			# print(d)
-			# print(word_id)
 			index[token] = d
 	return index
-
-def get_reddit_comment_as_dict(id):
-	print("ID: " + str(id))
-	"""
-	Given a comment id, queries reddit API for the info and
-	returns a json comment containing the body, author,
-	score, upvotes/downvotes, subreddit, its permalink and
-	the number of gilds it has
-	"""
-	comment = Comment.query.filter_by(comment_id=id).first()
-	if comment is None:
-		return None
-	return cvt_Comment_to_dict(comment)
-
-def cvt_Comment_to_dict(comment):
-	comment_dict = {}
-	comment_dict["id"] = comment.comment_id
-	comment_dict["body"] = comment.body
-	comment_dict["author"] = comment.author
-	comment_dict["score"] = comment.score
-	comment_dict["ups"] = comment.upvotes
-	comment_dict["downs"] = comment.downvotes
-	comment_dict["subreddit"] = comment.subreddit
-	comment_dict["permalink"] = "t1_" + comment.comment_id
-	comment_dict["gilded"] = comment.gilded
-	comment_dict["link_id"] = comment.link_id
-	return comment_dict
 
 def index_search(query_tokens, index, idf, doc_norms):
 	""" Search the collection of documents for the given query
@@ -153,3 +150,32 @@ def index_search(query_tokens, index, idf, doc_norms):
 	output = sorted(filtered_scores.items(), key=lambda x: x[1], reverse=True)[:20]
 
 	return [id_to_comment[str(comment[0])] for comment in output]
+
+################################ HELPER FUNCTIONS ##################################
+
+def get_reddit_comment_as_dict(id):
+	print("ID: " + str(id))
+	"""
+	Given a comment id, queries reddit API for the info and
+	returns a json comment containing the body, author,
+	score, upvotes/downvotes, subreddit, its permalink and
+	the number of gilds it has
+	"""
+	comment = Comment.query.filter_by(comment_id=id).first()
+	if comment is None:
+		return None
+	return cvt_Comment_to_dict(comment)
+
+def cvt_Comment_to_dict(comment):
+	comment_dict = {}
+	comment_dict["id"] = comment.comment_id
+	comment_dict["body"] = comment.body
+	comment_dict["author"] = comment.author
+	comment_dict["score"] = comment.score
+	comment_dict["ups"] = comment.upvotes
+	comment_dict["downs"] = comment.downvotes
+	comment_dict["subreddit"] = comment.subreddit
+	comment_dict["link_id"] = "https://www.reddit.com/" + comment.link_id[3:]
+	comment_dict["permalink"] = "http://www.reddit.com/comments/" + comment.link_id[3:] + "/_/" + comment.comment_id
+	comment_dict["gilded"] = comment.gilded
+	return comment_dict
