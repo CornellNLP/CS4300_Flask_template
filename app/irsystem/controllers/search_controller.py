4e2ae8999a77e4f701657677d3b53686d4ad7bb1
Seyun <sk956@cornell.edu>
2018-04-23 23:48:26 -0400

    updated file

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -140,105 +140,105 @@ def put_books_in_db(hash_factor = 100):
 	print('commited!')
 
 
-# @irsystem.route('/', methods=['GET'])
-# def delandadd():
-# 	empty_db()
-# 	create_tables()
-# 	put_books_in_db()
-# 	word_cloud_message = ''
-# 	top_books_message = ''
-# 	word_cloud = ['successfully added']
-# 	top_books = ['successfully added']
-# 	available_words = []
-# 	available_books = []
-# 	return render_template('search.html', name=project_name, netid=net_id, word_cloud_message=word_cloud_message, top_books_message=top_books_message, word_cloud=word_cloud, top_books = top_books)
-
-
 @irsystem.route('/', methods=['GET'])
-def search():
-	available_words = json.load(open('words.json'))
-	available_words = [unicodedata.normalize('NFKD', w).encode('ascii','ignore') for w in available_words]
-	available_books = json.load(open('books.json'))
-	available_books = [unicodedata.normalize('NFKD', b).encode('ascii','ignore') for b in available_books]
-
-	title_input = request.args.get('title_search')
-	keyword_input = request.args.get('keyword_search')
-
-	book_to_index = json.load(open("book_to_index.json"))
-	book_to_index = {key.strip() : value for key, value in book_to_index.iteritems()}
-
-
-	word_to_index = json.load(open("word_to_index.json"))
-	book_image_url =json.load(open("ISBN_100000_to_200000.json"))
+def delandadd():
+	empty_db()
+	create_tables()
+	put_books_in_db()
+	word_cloud_message = ''
+	top_books_message = ''
+	word_cloud = ['successfully added']
+	top_books = ['successfully added']
+	available_words = []
+	available_books = []
+	return render_template('search.html', name=project_name, netid=net_id, word_cloud_message=word_cloud_message, top_books_message=top_books_message, word_cloud=word_cloud, top_books = top_books)
 
 
+# @irsystem.route('/', methods=['GET'])
+# def search():
+# 	available_words = json.load(open('words.json'))
+# 	available_words = [unicodedata.normalize('NFKD', w).encode('ascii','ignore') for w in available_words]
+# 	available_books = json.load(open('books.json'))
+# 	available_books = [unicodedata.normalize('NFKD', b).encode('ascii','ignore') for b in available_books]
 
-	if title_input == None and keyword_input == None:
-		word_cloud_message = ''
-		top_books_message =  ''
-		word_cloud = []
-		top_books = []
+# 	title_input = request.args.get('title_search')
+# 	keyword_input = request.args.get('keyword_search')
 
-	elif keyword_input is not None: 
-		word_cloud_message = ''
-		word_cloud = []
-		top_books = []
-		keywords = keyword_input.split(' ')
-		rel_keywords = []
-		for keyword in keywords:
-			if keyword not in word_to_index.keys():
-				top_books.append(keyword + " is not in our database.")
-			else:
-				rel_keywords.append(keyword)
-		if len(top_books) == len(keywords): 
-			top_books_message = 'All the keywords are not in our database.'
-		else:
-			top_books_message = "Top 10 books for the keyword are:"
-			word_list = []
-			ith_list = []
-			print(keyword_input)
-			for keyword in rel_keywords:
-				i = word_to_index[keyword]
-				w = Words.query.filter_by(start_index = int(i)/100*100).first()
-				word_list.append(w)
-				ith_list.append(int(i)%100) 
-			for close_book in db_word_to_closest_books(word_list, ith_list):
-				each_book_list =[]
-				each_book_list.append(close_book)
-				if close_book in book_image_url:
-					print("hellothere")
-					isbn= book_image_url[close_book]
-					print("notisbn")
-					newisbn =[]
-					for nums in isbn[:2] : 
-						url = "http://covers.openlibrary.org/b/isbn/" + nums +"-M.jpg"
-						url=url.encode('ascii','ignore')
-						print(url)
-						newisbn.append(url)
-					link=isbn[2].encode('ascii','ignore')
-					link ="http://www.goodreads.com/book/show/" + link
-					newisbn.append(link)
-					for nounicode in newisbn: 
-						each_book_list.append(nounicode)
+# 	book_to_index = json.load(open("book_to_index.json"))
+# 	book_to_index = {key.strip() : value for key, value in book_to_index.iteritems()}
 
-					each_book_list.append([])
-				else: 
-					errorlist = ["", "",""]
-					each_book_list += errorlist
+
+# 	word_to_index = json.load(open("word_to_index.json"))
+# 	book_image_url =json.load(open("ISBN_100000_to_200000.json"))
+
+
+
+# 	if title_input == None and keyword_input == None:
+# 		word_cloud_message = ''
+# 		top_books_message =  ''
+# 		word_cloud = []
+# 		top_books = []
+
+# 	elif keyword_input is not None: 
+# 		word_cloud_message = ''
+# 		word_cloud = []
+# 		top_books = []
+# 		keywords = keyword_input.split(' ')
+# 		rel_keywords = []
+# 		for keyword in keywords:
+# 			if keyword not in word_to_index.keys():
+# 				top_books.append(keyword + " is not in our database.")
+# 			else:
+# 				rel_keywords.append(keyword)
+# 		if len(top_books) == len(keywords): 
+# 			top_books_message = 'All the keywords are not in our database.'
+# 		else:
+# 			top_books_message = "Top 10 books for the keyword are:"
+# 			word_list = []
+# 			ith_list = []
+# 			print(keyword_input)
+# 			for keyword in rel_keywords:
+# 				i = word_to_index[keyword]
+# 				w = Words.query.filter_by(start_index = int(i)/100*100).first()
+# 				word_list.append(w)
+# 				ith_list.append(int(i)%100) 
+# 			for close_book in db_word_to_closest_books(word_list, ith_list):
+# 				each_book_list =[]
+# 				each_book_list.append(close_book)
+# 				if close_book in book_image_url:
+# 					print("hellothere")
+# 					isbn= book_image_url[close_book]
+# 					print("notisbn")
+# 					newisbn =[]
+# 					for nums in isbn[:2] : 
+# 						url = "http://covers.openlibrary.org/b/isbn/" + nums +"-M.jpg"
+# 						url=url.encode('ascii','ignore')
+# 						print(url)
+# 						newisbn.append(url)
+# 					link=isbn[2].encode('ascii','ignore')
+# 					link ="http://www.goodreads.com/book/show/" + link
+# 					newisbn.append(link)
+# 					for nounicode in newisbn: 
+# 						each_book_list.append(nounicode)
+
+# 					each_book_list.append([])
+# 				else: 
+# 					errorlist = ["", "",""]
+# 					each_book_list += errorlist
 					
-				top_books.append(each_book_list)
+# 				top_books.append(each_book_list)
 
 
-	else:
-		top_books_message = ""
-		top_books = []
+# 	else:
+# 		top_books_message = ""
+# 		top_books = []
 
-		if title_input not in book_to_index.keys():
-			word_cloud_message = ''
-			word_cloud = ['The book is not in our database.']
-		else:
-			i = book_to_index[title_input]
-			b = Books.query.filter_by(start_index = int(i)/100*100).first()
-			word_cloud_message = 'Word cloud is: '	
-			word_cloud = db_book_to_closest_words(b, int(i) % 100)
-	return render_template('search.html', name=project_name, netid=net_id, word_cloud_message=word_cloud_message, top_books_message=top_books_message, word_cloud=word_cloud, top_books = top_books, avail_keywords = available_words, avail_books = available_books)
+# 		if title_input not in book_to_index.keys():
+# 			word_cloud_message = ''
+# 			word_cloud = ['The book is not in our database.']
+# 		else:
+# 			i = book_to_index[title_input]
+# 			b = Books.query.filter_by(start_index = int(i)/100*100).first()
+# 			word_cloud_message = 'Word cloud is: '	
+# 			word_cloud = db_book_to_closest_words(b, int(i) % 100)
+# 	return render_template('search.html', name=project_name, netid=net_id, word_cloud_message=word_cloud_message, top_books_message=top_books_message, word_cloud=word_cloud, top_books = top_books, avail_keywords = available_words, avail_books = available_books)
