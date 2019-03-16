5ffbf9bf5aece91975eac25e3dbcdb9e62d7c195
bn13565 <bkn7@cornell.edu>
2019-03-16 16:09:12 -0400

    my first commit

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -2,8 +2,8 @@ from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
-project_name = "Ilan's Cool Project Template"
-net_id = "Ilan Filonenko: if56"
+project_name = "Bryan's Cool Project Template"
+net_id = "Bryan Kamau: bkn7"
 
 @irsystem.route('/', methods=['GET'])
 def search():
