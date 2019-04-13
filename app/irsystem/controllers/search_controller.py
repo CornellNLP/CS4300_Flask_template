43cc1234bded4ac2794628c10439d0eeb71b4c68
Caroline Chang <cdc222@cornell.edu>
2019-04-13 17:17:20 -0400

    prompt should be accessible in results.html as well as backend

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -8,6 +8,15 @@ project_name = "Get StartTED: TED Talk Recommendation System"
 # net_id = "Andrea Benson ab2393, Caroline Chang cdc222, Nandita Mohan nkm39, Gauri Jain gj82, Michael Rivera mr858"
 net_id = "Andrea Benson, Caroline Chang, Nandita Mohan, Gauri Jain, Michael Rivera"
 
+def process_single_prompt(url): #functionality could be in a js file as well
+	url_parts = url.split('=')
+	prompt = url_parts[1]
+	words = prompt.split('+')
+	final_str = ""
+	for w in words:
+		final_str = final_str + " " + w
+	return final_str
+
 @irsystem.route('/', methods=['GET'])
 def search():
 	query = request.args.get('search')
@@ -18,4 +27,5 @@ def search():
 
 	else:
 		output_message = query
-		return render_template('results.html', output_message=output_message)
+		prompt1 = process_single_prompt(request.url)
+		return render_template('results.html', output_message=output_message, prompt=prompt1)
