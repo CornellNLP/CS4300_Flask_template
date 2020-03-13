475ed1d16d33c8a872ca162fbd8527a76967101b
Genghis Shyy <gs484@cornell.edu>
2020-03-12 23:11:53 -0400

    Updating home page with netids

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,20 +1,18 @@
-from . import *  
+from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
-project_name = "Ilan's Cool Project Template"
-net_id = "Ilan Filonenko: if56"
+project_name = "Fitness Dream Team"
+net_ids = "Genghis Shyy: gs484, Henri Clarke: hxc2, Alice Hu: ath84, Michael Pinelis: mdp93, Sam Vacura: smv66"
+
 
 @irsystem.route('/', methods=['GET'])
 def search():
-	query = request.args.get('search')
-	if not query:
-		data = []
-		output_message = ''
-	else:
-		output_message = "Your search: " + query
-		data = range(5)
-	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
-
-
-
+    query = request.args.get('search')
+    if not query:
+        data = []
+        output_message = ''
+    else:
+        output_message = "Your search: " + query
+        data = range(5)
+    return render_template('search.html', name=project_name, netid=net_ids, output_message=output_message, data=data)
