78345ffa1c4ed46bf2ab3ef9a91e80e2912cb434
vineetparikh <vince.parikh@gmail.com>
2020-03-21 17:51:07 -0400

    edit title and netids

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,9 +1,9 @@
-from . import *  
+from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
-project_name = "Ilan's Cool Project Template"
-net_id = "Ilan Filonenko: if56"
+project_name = "Character Crafter: Turn DnD Concepts to DnD Characters"
+net_id = "Vineet Parikh (vap43), Matthew Shih (ms2628), Eli Schmidt (es797), Eric Sunderland(evs37), Eric Chen(ebc48)"
 
 @irsystem.route('/', methods=['GET'])
 def search():
@@ -15,6 +15,3 @@ def search():
 		output_message = "Your search: " + query
 		data = range(5)
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
-
-
-
