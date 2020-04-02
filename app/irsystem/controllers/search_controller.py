dcf17ae0d00d86a3fb5061b2697e1ebe4d458122
Jason Jung <jj634@cornell.edu>
2020-04-02 17:53:37 -0400

    now with our netids

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -2,8 +2,8 @@ from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
-project_name = "Ilan's Cool Project Template"
-net_id = "Ilan Filonenko: if56"
+project_name = "Haha Factory"
+net_id = "Jason Jung: jj634, Suin Jung: sj575, Winice Hui: wh394, Cathy Xin: cyx5, Rachel Han: ryh25"
 
 @irsystem.route('/', methods=['GET'])
 def search():
