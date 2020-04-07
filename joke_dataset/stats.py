336b2836e79559d53756d0e53791809cfb148dee
Winice Hui <winice.hui@gmail.com>
2020-04-06 17:39:47 -0700

    removed misc

--- joke_dataset/stats.py
+++ joke_dataset/stats.py
@@ -1,16 +1,27 @@
 from glob import glob
 import json
 
-cat_list = {}
-source_list = {}
-num = 0
+cat_list = {} #dictionary w/ (key, value) = (category name, number of jokes in category)
+source_list = {} #dictionary w/ (key, value) = (source, number of jokes from source)
+total_jokes = 0 #total number of jokes in dataset
+
+num_unclassified = 0 #total number of jokes that are unclassified
+num_noscore = 0 #total number of jokes that do not have a score 
+
 for filename in glob('./json/*.json'): #loop over .json files
     with open(filename) as f: 
         data = json.load(f) #open the json file 
         source_num = 0
         for obj in data:
             source_num = source_num + 1
-            num = num + 1
+            total_jokes += 1
+            if len(obj['categories']) == 0:
+                num_unclassified += 1
+                continue
+
+            if obj['score'] is None: 
+                num_noscore +=1
+
             for cat in obj['categories']: 
                 if cat not in cat_list.keys():
                     cat_list[cat] = 1
@@ -22,17 +33,21 @@ for filename in glob('./json/*.json'): #loop over .json files
 cat_list = sorted(cat_list.items(), key=lambda item: item[0])
 source_list = sorted(source_list.items(), key=lambda item: item[0])
 
-f = open("JokeStats.txt", "w+")
-str1 = "Number of Jokes: " + str(num) + "\n"
+f = open("stats.txt", "w+")
+
+str1 = "Total Number of Jokes: " + str(total_jokes) + "\n"
 f.write(str1)
+
+f.write("\nNumber of jokes without categories: \n" + str(num_unclassified) + "\n")
 f.write("\nCategories: \n")
 for item in cat_list:
-    str2 = item[0] + ": "+ str(item[1]) + " jokes \n"
-    f.write(str2)
+    s = item[0] + ": "+ str(item[1]) + " jokes \n"
+    f.write(s)
 
+f.write("\nNumber of jokes without scores: \n" +str(num_noscore) + "\n")
 f.write("\nSources: \n")
 for item in source_list:
-    str2 = item[0] + ": " + str(item[1]) + " jokes \n"
-    f.write(str2)
+    s = item[0] + ": " + str(item[1]) + " jokes \n"
+    f.write(s)
 
 f.close()
