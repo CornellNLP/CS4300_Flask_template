3023a4a192551606be9a26c005f4218a3e2a12e9
Cathy <xinycath@gmail.com>
2020-04-08 00:38:44 -0400

    added more json and jokes

---
+++ joke_dataset/sunnyskyz_puns.py
@@ -0,0 +1,28 @@
+from lxml import html
+from lxml.cssselect import CSSSelector
+import requests
+import json
+import re
+
+def extract_jokes():
+    result = []
+
+    url = 'https://www.sunnyskyz.com/funny-jokes/221/58-Funny-Puns-You-Can-t-Wait-To-Use'
+    response = requests.get(url)
+
+    sel = CSSSelector('#picofday p')
+
+    html_elmnts = html.fromstring(response.content)
+
+    for joke in sel(html_elmnts):
+        result.append({'joke': joke.text_content().strip(), 'score': None, 'categories': ['Pun']})
+
+    return result
+
+jokes = []
+
+try :
+    jokes = extract_jokes()
+finally:
+    with open('./json/sunnyskyz_puns_raw.json', 'w') as f:
+        json.dump(jokes, f, indent=4)
