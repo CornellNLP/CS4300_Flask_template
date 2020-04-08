f5d4eaa76ad38bda0df567270e8c8c838aed61d2
Cathy <xinycath@gmail.com>
2020-04-08 00:01:10 -0400

    finished rd_puns and added boredpanda, ran stats again

--- joke_dataset/rd_puns_jokes.py
+++ joke_dataset/rd_puns_jokes.py
@@ -9,11 +9,22 @@ def extract_jokes(page):
     url = 'https://www.rd.com/jokes/puns/page/{}/'
     response = requests.get(url.format(page))
 
-    sel = CSSSelector('#genesis-content p')
+    sel = CSSSelector('.excerpt-wrapper')
 
     html_elmnts = html.fromstring(response.content)
 
     for joke in sel(html_elmnts):
-        print(joke.text_content())
+        result.append({'joke':joke.text_content().strip(), 'score': None, 'categories': ['Pun']})
 
-extract_jokes(1)
+    return result
+
+jokes = []
+
+try :
+    for i in range(50):
+        joke_lst = extract_jokes(i)
+        for joke in joke_lst:
+            jokes.append(joke)
+finally:
+    with open('./json/rd_puns_jokes_raw.json', 'w') as f:
+        json.dump(jokes, f, indent=4)
