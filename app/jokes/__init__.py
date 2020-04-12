ce0d5e866056671111d4f8bab8e8aec80b6032ce
Winice Hui <winice.hui@gmail.com>
2020-04-12 12:23:07 -0700

    Insert final.json into PostgreSQL
    - Set up database according to instructions
    - Migrate, upgrade
    - Run python insert.py

--- app/jokes/__init__.py
+++ app/jokes/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 
 # Define a Blueprint for this module (mchat)
+# note: __name__ is set to __main__ if executed as the main program. But if imported from another module, it is set to module's name (jokes)
 jokes = Blueprint('jokes', __name__, url_prefix='/jokes')
 
 # Import all controllers
