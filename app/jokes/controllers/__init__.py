9252c1921db05d21f2e5717157c3ea2b6f0468bb
Jason Jung <jj634@cornell.edu>
2020-04-13 01:55:14 -0400

    some routing

--- app/jokes/controllers/__init__.py
+++ app/jokes/controllers/__init__.py
@@ -1,6 +1,21 @@
-from functools import wraps
-from flask import request, jsonify, abort
-import os
+# Import flask deps
+from flask import request, render_template, \
+	flash, g, session, redirect, url_for, jsonify, abort
 
-# Import module models
+# For decorators around routes
+from functools import wraps 
+
+# Import the db object from main app module
+from app import db 
+
+# Marshmallow 
+from marshmallow import ValidationError
+
+# Import socketio for socket creation in this module 
+from app import socketio
+
+# Import module models 
 from ..models.joke import *
+
+# IMPORT THE BLUEPRINT APP OBJECT 
+from app.jokes import jokes 
