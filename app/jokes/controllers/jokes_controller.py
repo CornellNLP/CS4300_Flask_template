8dbc614c6957c3eed6a3661444ca57c32f1ff7d0
Jason Jung <jj634@cornell.edu>
2020-04-13 11:09:05 -0400

    removed migrations from gitignore, moved insert.py to toplevel

--- app/jokes/controllers/jokes_controller.py
+++ app/jokes/controllers/jokes_controller.py
@@ -11,24 +11,24 @@ from . import *
 
 @jokes.route('/', methods=['GET','POST'])
 def handle_jokes():
-    if request.method == 'POST':
-        if request.is_json:
-            data = request.get_json()
-            new_joke = Joke(text=data['text'], categories=data['categories'], score=data['score'], maturity=data['maturity'])
-            db.session.add(new_joke)
-            db.session.commit()
-            return {"message": "joke {new_joke.id} has been created successfully."}
-        else:
-            return {"error": "The request payload is not in JSON format"}
+  if request.method == 'POST':
+    if request.is_json:
+      data = request.get_json()
+      new_joke = Joke(text=data['text'], categories=data['categories'], score=data['score'], maturity=data['maturity'])
+      db.session.add(new_joke)
+      db.session.commit()
+      return {"message": "joke {new_joke.id} has been created successfully."}
+    else:
+      return {"error": "The request payload is not in JSON format"}
 
-    elif request.method == 'GET':
-        jokes = Joke.query.all()
-        results = [
-            {
-                "text": joke.text,
-                "categories": joke.categories,
-                "score": str(joke.score),
-                "maturity": joke.maturity,
-            } for joke in jokes]
+  elif request.method == 'GET':
+    jokes = Joke.query.all()
+    results = [
+      {
+        "text": joke.text,
+        "categories": joke.categories,
+        "score": str(joke.score),
+        "maturity": joke.maturity,
+      } for joke in jokes]
 
-        return {"count": len(results), "jokes": results}
+    return {"count": len(results), "jokes": results}
