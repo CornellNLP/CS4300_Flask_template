a8a42a871bd3a1460362f22337c969d9e5841b72
Dana Luong <dl697@cornell.edu>
2020-04-14 14:49:24 -0400

    add and tokenize data

--- embeddings/data_tools.py
+++ embeddings/data_tools.py
@@ -1,7 +1,26 @@
+import json
+import numpy as np
+import re
+from nltk.tokenize import TreebankWordTokenizer
 import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
 
-DATA_DIR = 'embeddings/data/'
+DATA_DIR = 'data/'
 DESC_MAP = pd.read_csv(DATA_DIR + 'descriptor_mapping.csv').set_index('raw descriptor')
+TOKENIZER = TreebankWordTokenizer()
+
+# Import wine data and extract reviews.
+def get_wine_data():
+    f=open(DATA_DIR+"winemag-data-130k-v2.json", "r")
+    transcripts = json.load(f)
+    transcripts = transcripts[0:25000]
+    return [t["description"] for t in transcripts]
+
+# Import beer data and extract reviews.
+def get_beer_data():
+    transcripts = pd.read_csv(DATA_DIR+"beer_train.csv").to_numpy()
+    transcripts = transcripts[0:25000]
+    return [t[4] for t in transcripts]
 
 def get_descriptor(word):
     if word in list(DESC_MAP.index):
@@ -10,3 +29,17 @@ def get_descriptor(word):
     else:
         return word
 
+# Returns a list of unique types that appear in the corpus.
+# @param data_list is a list of strings where each string is the review for a
+# beverage and the index of the string is the index of the beverage that the
+# review corresponds to.
+def tokenize(data_list):
+    bev_tokens = []
+    descr_rgx = "[a-z]+"
+    num_docs = len(data_list)
+    for doc_ind in range(0, num_docs):
+        tokens = set(TOKENIZER.tokenize(data_list[doc_ind].lower()))
+        fil_tokens = [x for x in tokens if re.match(descr_rgx, x)]
+        bev_tokens.append(fil_tokens)
+    types = list(set(token for token_list in bev_tokens for token in token_list))
+    return types
--- embeddings/train.py
+++ embeddings/train.py
@@ -1,7 +1,15 @@
 from gensim.models import Word2Vec
-from data_tools import get_descriptor
+from data_tools import get_descriptor, get_wine_data, get_beer_data, tokenize
+# from data_tools import get_wine_data
+# from data_tools import get_beer_data
+# from data_tools import tokenize
 
 def main():
+    wine_data = get_wine_data() # Sentence reviews about wines
+    wine_types = tokenize(wine_data) # Set of unique types that appear in sentences
+    beer_data = get_beer_data()
+    beer_types = tokenize(beer_data)
+
     sentences = [] # placeholder
 
     norm_sentences = []
@@ -11,6 +19,7 @@ def main():
             norm_w = get_descriptor(w)
             norm_s.append(norm_w)
         norm_sentences.append(norm_s)
-    
+
     model = Word2Vec(norm_sentences, size=300, min_count=5, iter=15)
-            
+
+main()
