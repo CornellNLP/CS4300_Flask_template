a60fd7608f5f3529754a786c9932dfdf900b358f
Zara Schreiber <zjs28@cornell.edu>
2020-04-14 12:15:27 -0400

    structures use id now

--- reddit-datasets/create_structures.py
+++ reddit-datasets/create_structures.py
@@ -6,22 +6,21 @@ import string
 import pickle
 import time
 import numpy as np
-# from nltk.tokenize import TreebankWordTokenizer
-# from IPython.core.display import HTML
-file_path_name = 'reddit-datasets/reddit-data-1-post-processed'
-file_path = file_path_name + ".json"
-
+from shared_variables import num_posts
+from shared_variables import jar
+from shared_variables import file_path
 """
 this file is to create datastructures. Currently creates:
 
 - inverted index
 """
 
-# LOAD DATA
-with open(file_path) as file:
-    data = json.load(file)
 
-num_docs = len(data)
+def load_data():
+    # open the already filtered dataset
+    with open(file_path) as file:
+        return json.load(file)
+
 
 """
   Makes an inverted index from given file
@@ -44,9 +43,8 @@ def make_inverted_index(file):
         for k, v in temp_dict.items():
             if k not in inv_index:
                 inv_index[k] = []
-            inv_index[k].append((i, temp_dict[k]))
+            inv_index[k].append((file[i]['id'], temp_dict[k]))
     return inv_index
-
 
 
 """
@@ -66,15 +64,14 @@ def get_idf(inv_index, num_docs, min_df=0, max_df=1):  # TODO: change these min/
     return idf
 
 
-
 """
   Computes norm of each document
-  Returns: np.array where norms[i] = norm of document i
+  Returns: dict where {doc_id: norm of doc}
 """
 
 
 def get_doc_norms(inv_index, idf, num_docs):
-    norms = np.zeros(num_docs)
+    norms = {}
 
     for k, v in inv_index.items():
         if k in idf:
@@ -82,19 +79,27 @@ def get_doc_norms(inv_index, idf, num_docs):
             for pair in v:
                 i = pair[0]
                 tf = pair[1]
-                norms[i] += (tf*idf_i)**2
+                if i not in norms:
+                    norms[i] = (tf*idf_i)**2
+                else:
+                    norms[i] += (tf*idf_i)**2
 
-    norms = np.sqrt(norms)
+    for k, v in norms.items():
+        norms[k] = v**(0.5)
 
     return norms
 
 
+def create_and_store_structures():
+    data = load_data()
+    num_docs = len(data)
 
-inverted_index = make_inverted_index(data)
-idf = get_idf(inverted_index, num_docs)
-norms = get_doc_norms(inverted_index, idf, num_docs)
+    inverted_index = make_inverted_index(data)
+    idf = get_idf(inverted_index, num_docs)
+    norms = get_doc_norms(inverted_index, idf, num_docs)
 
-jar = "reddit-datasets/picklejar/"
-pickle.dump(inverted_index, open(jar + "inverted_index.pickle", 'wb'))
-pickle.dump(idf, open(jar + "idf.pickle", 'wb'))
-pickle.dump(norms, open(jar + "norms.pickle", 'wb'))
+    # store data in pickle files
+    pickle.dump(inverted_index, open(
+        jar + str(num_posts) + "-inverted_index.pickle", 'wb'))
+    pickle.dump(idf, open(jar + str(num_posts) + "-idf.pickle", 'wb'))
+    pickle.dump(norms, open(jar + str(num_posts) + "-norms.pickle", 'wb'))
