b9e9149d50531ff456cbc3b69e334d00ecb79806
Zara Schreiber <zjs28@cornell.edu>
2020-04-14 12:16:46 -0400

    updated ids

---
+++ reddit-datasets/shared_variables.py
@@ -0,0 +1,5 @@
+jar = "reddit-datasets/picklejar/"
+num_posts = 100
+file_path_name = jar + 'reddit-data-' + str(num_posts) + '-posts-processed'
+file_path = file_path_name + ".json"
+original_dataset_path = 'reddit-datasets/picklejar/reddit-data-' + str(num_posts) + '-posts.json'
