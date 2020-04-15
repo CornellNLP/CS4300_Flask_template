604bb92fd017f595f4f89f28b1e05dd70667d845
Zara Schreiber <zjs28@cornell.edu>
2020-04-15 17:52:01 -0400

    more front end fun times

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,4 +1,4 @@
-from . import *  
+from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 from app.irsystem.models.search import full_search
@@ -6,16 +6,15 @@ from app.irsystem.models.search import full_search
 project_name = "Subreddit Recommender"
 class_name = "CS 4300 Spring 2020"
 
+
 @irsystem.route('/', methods=['GET'])
 def search():
-	query = request.args.get('search')
-	if not query:
-		data = []
-		output_message = ''
-	else:
-		output_message = "Your search: " + query
-		data = full_search(query)
-	return render_template('search.html', name=project_name, class_name=class_name, output_message=output_message, data=data)
-
-
-
+    query = request.args.get('search')
+    if not query:
+        data = []
+        output_message = ''
+    else:
+        # output_message = "Your search: " + query
+        output_message = "You might want to post in:"
+        data = full_search(query)
+    return render_template('search.html', name=project_name, class_name=class_name, output_message=output_message, data=data)
