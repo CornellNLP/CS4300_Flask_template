094eec515d2c38a00c0db717683c93bfce1e795d
Collin Montag <cm759@cornell.edu>
2020-04-16 11:36:49 -0400

    ignore env

--- env.bat
+++
@@ -1,4 +0,0 @@
-:: Set the environment type of the app (see config.py)
-SET APP_SETTINGS=config.DevelopmentConfig
-:: Set the DB url to a local database for development
-SET DATABASE_URL=postgresql://localhost/my_app_db
