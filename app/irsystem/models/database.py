58fa061a97fcd9b6a64ff519fdbe7e1450efba11
Collin Montag <cm759@cornell.edu>
2020-04-16 11:31:19 -0400

    drink vectorizer

--- app/irsystem/models/database.py
+++ app/irsystem/models/database.py
@@ -4,13 +4,19 @@ class Drink(db.Model):
     __tablename__ = 'test'
     name = db.Column(db.String(64), primary_key=True)
     description = db.Column(db.Text(), nullable=False)
-    vector = db.Column(db.LargeBinary(), nullable=False)
+    vbytes = db.Column(db.LargeBinary(), nullable=False)
 
     def __repr__(self):
         return '<Drink {}>'.format(self.name)
 
-def add_drink(name, desc, v_bytes):
-    d = Drink(name=name, description=desc, vector=v_bytes)
-    db.session.add(d)
+# Add a `Drink` object to database
+def add_drink(drink):
+    db.session.add(drink)
+    db.session.commit()
+
+# Add a list of `Drink` objects to database
+def add_drink_batch(drinks):
+    for d in drinks:
+        db.session.add(d)
     db.session.commit()
     
--- embeddings/train.py
+++ embeddings/train.py
@@ -1,12 +1,18 @@
 from gensim.models import Word2Vec
 from data_tools import get_descriptor, get_wine_data, get_beer_data, tokenize
+from sklearn.feature_extraction.text import TfidfVectorizer
+from app.irsystem.models.database import add_drink_batch, Drink
+import numpy as np
 
 def main():
     wine_data = tokenize(get_wine_data())
     beer_data = tokenize(get_beer_data())
 
-    sentences = [] # placeholder
+    # TODO(@dana) List of word lists (lol) for each sentence in corpus after
+    # removing stop words, normalizing tokens, and phrasing using n-grams
+    sentences = []
 
+    # Replace tokens with descriptors when possible
     norm_sentences = []
     for s in sentences:
         norm_s = []
@@ -16,5 +22,27 @@ def main():
         norm_sentences.append(norm_s)
 
     model = Word2Vec(norm_sentences, size=300, min_count=5, iter=15)
+
+    # List of descriptor words for each drink description
+    drink_descs = []
+    vectorizer = TfidfVectorizer()
+    tfidf = vectorizer.fit(drink_descs)
+    tfidf_dict = dict(zip(tfidf.get_feature_names(), tfidf.idf_))
+
+    drinks = []
+    for d in drink_descs:
+        tokens = d.split(' ')
+        word_vectors = []
+        for t in tokens:
+            if t in tfidf_dict.keys():
+                weight = tfidf_dict[t]
+                vector = model.wv.get_vector(t)
+                word_vectors.append(vector * weight)
+        try:
+            drink_vector = sum(word_vectors) / len(word_vectors)
+        except:
+            drink_vector = np.array([])
+        drink = Drink(name='', description='', vbytes=drink_vector.tobytes())
+    add_drink_batch(drinks)
 
 # main()
