7414848a66575e3745e86ad3c3731afb6a84fa0a
Jason Jung <jj634@cornell.edu>
2020-04-18 18:49:57 -0400

    some basic form handling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,38 +12,79 @@ import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button'
 
 
-function App() {
-  return (
+class App extends React.Component {
 
-    <Container>
-      <Row className="justify-content-md-center">
-        <Col>
-          <header className="App-header">
-            <h1>HahaFactory</h1>
-            <img src={logo} className="App-logo" alt="logo" />
-          </header>
-          <Form className="global-search">
+  constructor(props) {
+    super(props)
+    this.state = {
+      isLoaded: false,
+      items: []
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-            <Form.Group controlId="Key Words" className="formGroupCenter">
-              <Form.Control
-                type="text"
-                name="name"
-                // value={this.state.newRequest.contactinfo.name}
-                // onChange={this.handleInput_contact}
-                placeholder="Enter key words..."
-                required
-              />
-            </Form.Group>
+  handleSubmit(event) {
+    event.preventDefault();
+    const data = new FormData(event.target);
 
-            <Button type="submit" class="btn btn-info">Go!</Button>
-          </Form>
+    fetch('/jokes/api', {
+      method: 'GET',
+      // body: data,
+    })
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.items
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
 
-        </Col>
-      </Row>
-    </Container >
+  render() {
+    return (
+
+      <Container>
+        <Row className="justify-content-md-center">
+          <Col>
+            <header className="App-header">
+              <h1>HahaFactory</h1>
+              <img src={logo} className="App-logo" alt="logo" />
+            </header>
+            <Form className="global-search" onSubmit={this.handleSubmit}>
+
+              <Form.Group controlId="Key Words" className="formGroupCenter">
+                <Form.Control
+                  type="text"
+                  name="name"
+                  // value={this.state.newRequest.contactinfo.name}
+                  // onChange={this.handleInput_contact}
+                  placeholder="Enter key words..."
+                  required
+                />
+              </Form.Group>
+
+              <Button type="submit" class="btn btn-info">Go!</Button>
+            </Form>
+
+          </Col>
+        </Row>
+      </Container >
 
 
-  );
+    );
+  }
+
 }
 
 export default App;
