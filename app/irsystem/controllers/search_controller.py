a8d2218984495dd5dbff89b20c63e4fbf7ffc22b
Haley Mathews <hcm58@cornell.edu>
2020-04-18 16:48:52 -0700

    uploaded sentiment word bank, attempting to change search_controller file

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,9 +1,10 @@
-from . import *  
+from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
+@import 'data_code/sent_comment_data.py';
 
-project_name = "Ilan's Cool Project Template"
-net_id = "Ilan Filonenko: if56"
+project_name = "NJ, Sophia, Jacob, & Haley's Project"
+net_id = "hcm58, sia9, ns633,"
 
 @irsystem.route('/', methods=['GET'])
 def search():
@@ -17,4 +18,16 @@ def search():
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
 
 
+@irsystem.route('/', methods=['GET'])
+def search():
+	query = request.args.get('search')
+	states = state_list
+	if query not in states:
+		output_message = query + ": Invalid State Name"
+	else:
+		sent_data = comment_sentiment
+		state_sentiment = sent_data[query]
+		output_message = state_sentiment
 
+		data = range(5)
+	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
