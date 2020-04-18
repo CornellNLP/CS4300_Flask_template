920b32360157699b8bb4f8ed9866e74b8134ce50
navinramsaroop <nr292@cornell.edu>
2020-04-17 21:37:15 -0400

    deploy

--- app/irsystem/models/shared_variables.py
+++ app/irsystem/models/shared_variables.py
@@ -2,10 +2,10 @@ import os
 
 this_dir = os.path.dirname(os.path.abspath(__file__))
 jar = this_dir + "/picklejar/"
-num_posts = 1000
+num_posts = 500
 num_subreddits = 1075
-min_words_per_post = 10
-max_document_frequency = 0.10
+min_words_per_post = 15
+max_document_frequency = 0.09
 """
 DATASET NAMING CONVENTION
 ""<num posts>p<num subreddits>s<min words per post>mwc.json"
