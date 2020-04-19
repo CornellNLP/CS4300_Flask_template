28bd0ffb7c2ae40b4e954dda4fc88c1e73ab5511
Winice Hui <winice.hui@gmail.com>
2020-04-18 18:43:04 -0700

    fix search_Controller

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -6,15 +6,16 @@ import app.irsystem.controllers.sim_lib as sl
 
 project_name = "Haha Factory"
 net_id = "Jason Jung: jj634, Suin Jung: sj575, Winice Hui: wh394, Cathy Xin: cyx5, Rachel Han: ryh25"
+dictionary = {}
 
 @irsystem.route('/', methods=['GET'])
 def search():
 	query = request.args.get('search')
 	min_score = request.args.get('score')
-        categories = None # request.args.get('categories')
+    # categories = None # request.args.get('categories')
 
 	if min_score is not None: 
-		jokes = Joke.query.filter_by(score >=  min_score).all()
+		jokes = Joke.query.filter(Joke.score >=  min_score).all()
 		results = [
 			{
         "text": joke.text,
@@ -23,21 +24,21 @@ def search():
         "maturity": joke.maturity,
 		} for joke in jokes]
 	
-        # uncomment for jaccard sim on categories
-        """ 
-        results_cat = []
-        if categories is not None:
-            cat_jokes = {} 
-            for cat in categories:
-                doc_lst = Joke.query.filter_by(category = cat).first()
-                cat_jokes[cat] = doc_lst
+        # # uncomment for jaccard sim on categories
+        # """ 
+        # results_cat = []
+        # if categories is not None:
+        #     cat_jokes = {} 
+        #     for cat in categories:
+        #         doc_lst = Joke.query.filter_by(category = cat).first()
+        #         cat_jokes[cat] = doc_lst
 
-            numer_dict = sl.get_rel_jokes(query, cat_jokes)
-            rel_jokes = {}
-            for doc in get_rel_jokes:
-                rel_jokes[doc] = Joke.query.filter_by(id = doc).first()
-            result_cat = jaccard_sim(numer_dict, rel_jokes)
-        """
+        #     numer_dict = sl.get_rel_jokes(query, cat_jokes)
+        #     rel_jokes = {}
+        #     for doc in get_rel_jokes:
+        #         rel_jokes[doc] = Joke.query.filter_by(id = doc).first()
+        #     result_cat = jaccard_sim(numer_dict, rel_jokes)
+        # """
 
 	Joke.testFunct()
 
