59db14408b5923a1e550560dfbdd86501c88f703
Winice Hui <winice.hui@gmail.com>
2020-04-20 00:10:07 -0700

    weighting

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -56,26 +56,33 @@ def search():
 			doc_id = element[0]
 			joke = Joke.query.filter_by(id = doc_id).first()
 			sim_measure = element[1]
-			results_cos[doc_id] = ({"text": joke.text, "categories": joke.categories,"score": str(joke.score),"maturity": joke.maturity}, sim_measure*0.5
-	
-	results = {}
+			results_cos[doc_id] = ({"text": joke.text, "categories": joke.categories,"score": str(joke.score),"maturity": joke.maturity}, sim_measure*0.5)
+
+	results_final = {}
 	for i in range(6408):
-		if i in results_cat and results_cos:
-			results[i] = results_cat[i][0], results_cat[i][1]*0.5 + results_cos[i][1]*0.5
-		elif i in results_cat:
-			results[i] = results_cat[i][0], results_cat[i][1] * 0.5
+		if i in results and i in results_cos:
+			results_final[i] = results[i][0], results[i][1]*0.5 + results_cos[i][1]*0.5
+		elif i in results:
+			results_final[i] = results[i][0], results[i][1] * 0.5
 		elif i in results_cos:
-			results[i] = results_cos[i][0], results_cos[i][1] * 0.5
+			results_final[i] = results_cos[i][0], results_cos[i][1] * 0.5
 
 	final = []
 	if min_score:
-		for joke in results:
-			if joke[0]['score'] >= min_score:
-				final.append((joke[0], "Similarity: " + str(joke[1]*0.67 + (0.33*float(joke[0]['score'])))))
+		for joke in results_final:
+			if results_final[joke][0]['score'] == 'None':
+				print("None")
+				final.append((results_final[joke][0], "Similarity: " + str(results_final[joke][1]*0.67)))
 			else:
-				final.append((joke[0], "Similarity: " + str(joke[1]*0.67 + (0.16*float(joke[0]['score'])))))
+				if float(results_final[joke][0]['score']) >= float(min_score):
+					final.append((results_final[joke][0], "Similarity: " + str(results_final[joke][1]*0.67 + (0.33/5*float(results_final[joke][0]['score'])))))
+				else:
+					final.append((results_final[joke][0], "Similarity: " + str(results_final[joke][1]*0.67 + (0.16/5*float(results_final[joke][0]['score'])))))
+				
 	else:
-		final = [(x[1][0], "Similarity: " + str(x[1][1])) for x in results.items()]
+		final = [(x[1][0], "Similarity: " + str(x[1][1])) for x in results_final.items()]
+
+	final = sorted(final, key = lambda x : (x[1]), reverse = True)
 
 	Joke.testFunct()
 
