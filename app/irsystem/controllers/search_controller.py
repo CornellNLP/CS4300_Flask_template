33e58cc56af5e933915de9c4c46189d0b3a66abb
Winice Hui <winice.hui@gmail.com>
2020-04-20 12:17:11 -0700

    categories as dropdown so won't run into internal server error

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -17,56 +17,58 @@ search_params = {}
 
 @irsystem.route('/', methods=['GET'])
 def search():
-    query = request.args.get('search')
-    min_score = request.args.get('score')
-    categories = request.args.get('category')
-
-    search_params['min_score'] = min_score if min_score else ''
-    search_params['categories'] = categories if categories else ''
-    search_params['key_words'] = query if query else ''
-
-    results_jac = {}  # dictionary key = joke_id, value = (joke_dict, jac_sim)
-
-    if categories:
-        categories_list = [el.strip() for el in categories.split(",")]
-
-        cat_jokes = {}  # dictionary where key = category, value = array of doc_ids with that category
-        for cat in categories_list:  # for every category
-            # get the record where category is equal to cat
-            doc_lst = Categories.query.filter_by(category=cat).first()
-            cat_jokes[cat] = doc_lst.joke_ids
+	cat_options = [cat.category for cat in Categories.query.all()]
+	query = request.args.get('search')
+	min_score = request.args.get('score')
+	categories = request.args.get('category')
+	
+	search_params['min_score'] = min_score if min_score else ''
+	search_params['categories'] = categories if categories else ''
+	search_params['key_words'] = query if query else ''
+	
+	results_jac = {}  # dictionary key = joke_id, value = (joke_dict, jac_sim)
+	
+	if categories:
+		categories_list = [el.strip() for el in categories.split(",")]
+		
+		cat_jokes = {}  # dictionary where key = category, value = array of doc_ids with that category
+		for cat in categories_list:  # for every category
+			 # get the record where category is equal to cat
+			doc_lst = Categories.query.filter_by(category=cat).first()
+			cat_jokes[cat] = doc_lst.joke_ids
 
         # dictionary with key = joke_id and value = numerator
-        numer_dict = jac.get_rel_jokes(cat_jokes)
-
-        rel_jokes = {}  # dictionary where key = joke_id, value = joke
-        for doc in numer_dict.keys():
-            rel_jokes[doc] = Joke.query.filter_by(id=doc).first()
-
-        results_cat = jac.jaccard_sim(categories_list, numer_dict, rel_jokes)
-
-        for element in results_cat:
-            doc_id = element[0]
-            joke = rel_jokes[doc_id]
-            sim_measure = (element[1])
-            if doc_id not in results_jac:
-                results_jac[doc_id] = ({"text": joke.text, "categories": joke.categories, "score": str(
-                    joke.score), "maturity": joke.maturity}, sim_measure)
+		numer_dict = jac.get_rel_jokes(cat_jokes)
+		
+		rel_jokes = {}  # dictionary where key = joke_id, value = joke
+		for doc in numer_dict.keys():
+			rel_jokes[doc] = Joke.query.filter_by(id=doc).first()
+		
+		results_cat = jac.jaccard_sim(categories_list, numer_dict, rel_jokes)
+		
+		
+		for element in results_cat:
+			doc_id = element[0]
+			joke = rel_jokes[doc_id]
+			sim_measure = (element[1])
+			if doc_id not in results_jac:
+				results_jac[doc_id] = ({"text": joke.text, "categories": joke.categories, "score": str(
+					joke.score), "maturity": joke.maturity}, sim_measure)
 
     # dictionary where key= joke_id, value = (joke_dict, cos_sim)
-    results_cos = {}
-
-    if query:
+	results_cos = {}
+	
+	if query:
         # a list of (joke_id, cos_sim)
-        results_query = cos.fast_cossim(query, inv_idx_free)
-        for element in results_query:
-            doc_id = element[0]
-            joke = Joke.query.filter_by(id=doc_id).first()
-            sim_measure = element[1]
-            results_cos[doc_id] = ({"text": joke.text, "categories": joke.categories, "score": str(
-                joke.score), "maturity": joke.maturity}, sim_measure)
-
-    results = ressy.weight(results_jac, results_cos)
+		results_query = cos.fast_cossim(query, inv_idx_free)
+		for element in results_query:
+			doc_id = element[0]
+			joke = Joke.query.filter_by(id=doc_id).first()
+			sim_measure = element[1]
+			results_cos[doc_id] = ({"text": joke.text, "categories": joke.categories, "score": str(
+				joke.score), "maturity": joke.maturity}, sim_measure)
+	
+	results = ressy.weight(results_jac, results_cos)
 
     # for i in range(6408):
     # 	if i in results and i in results_cos:
@@ -75,21 +77,20 @@ def search():
     # 		results[i] = results_jac[i][0], results_jac[i][1] * 0.5
     # 	elif i in results_cos:
     # 		results[i] = results_cos[i][0], results_cos[i][1] * 0.5
-
-    final = None
-    if min_score:
-        final = ressy.adj_minscore(float(min_score), results)
-    else:
-        # translate results into list without weighting for min_score
-        final = [(x[1][0], "Similarity: " + str(x[1][1]))
-                 for x in results.items()]
+	
+	final = None
+	if min_score:
+		final = ressy.adj_minscore(float(min_score), results)
+	else:
+		# translate results into list without weighting for min_score
+		final = [(x[1][0], "Similarity: " + str(x[1][1]))  for x in results.items()]
 
     # sort results by decreasing sim
-    final = sorted(final, key=lambda x: (x[1]), reverse=True)
+	final = sorted(final, key=lambda x: (x[1]), reverse=True)
+	cat_options = sorted(cat_options)
 
     # Joke.testFunct()
-
-    return render_template('search.html', name=project_name, netid=net_id, output_message=search_params, data=final)
+	return render_template('search.html', name=project_name, netid=net_id, output_message=search_params, data=final, cat_options = cat_options)
 
 
 @irsystem.route('/react', methods=['GET'])
