a784708c9d6ed6f4b153eb6e320b6f397404d4eb
Collin Montag <cm759@cornell.edu>
2020-04-24 16:47:35 -0400

    added wine scrape

--- embeddings/scrape.sh
+++ embeddings/scrape.sh
@@ -1,3 +1,4 @@
 cd scraper
 scrapy crawl shack-beer -o ../data/shack-beer.json -t json
 scrapy crawl conno-beer -o ../data/conno-beer.json -t json
+scrapy crawl wcom-wine -o ../data/wcom-wine.json -t json
