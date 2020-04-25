1359cb302b9a7e97201aa880a97e70cb298f212c
Collin Montag <cm759@cornell.edu>
2020-04-25 00:52:53 -0400

    added cocktail scrape

--- embeddings/scrape.sh
+++ embeddings/scrape.sh
@@ -3,3 +3,4 @@ scrapy crawl shack-beer -o ../data/shack-beer.json -t json
 scrapy crawl conno-beer -o ../data/conno-beer.json -t json
 scrapy crawl wcom-wine -o ../data/wcom-wine.json -t json
 scrapy crawl kw-liquor -o ../data/kw-liquor.json -t json
+scrapy crawl spruce-cocktail -o ../data/spruce-cocktail.json -t json
