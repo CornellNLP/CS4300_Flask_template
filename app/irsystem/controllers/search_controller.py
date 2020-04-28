ba8a8d480c9c741e06a0bd33c7e3de5a8677519e
Collin Montag <cm759@cornell.edu>
2020-04-28 01:20:58 -0400

    working filters and paging

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -14,6 +14,9 @@ Dana Luong (dl697),
 Ishneet Sachar (iks23)
 """
 
+def arg_exists(arg):
+	return arg is not None and arg != ''
+
 @irsystem.route('/descriptors', methods=['GET'])
 def serve_desc():
 	descriptors = sorted([e.word for e in query_embeddings()])
@@ -24,19 +27,35 @@ def serve_desc():
 def search():
 
 	page_number = request.args.get('page')
+	page_number = int(page_number) if arg_exists(page_number) else 1
 	drink_type = request.args.get('type')
 	base = request.args.get('base')
 	descriptors = request.args.get('descriptors')
-	min_price = request.args.get('min-price')
-	max_price = request.args.get('max-price')
-	min_abv = request.args.get('min-abv')
-	max_abv = request.args.get('max-abv')
+	min_price = request.args.get('minprice')
+	min_price = float(min_price) if arg_exists(min_price) else None
+	max_price = request.args.get('maxprice')
+	max_price = float(max_price) if arg_exists(max_price) else None
+	min_abv = request.args.get('minabv')
+	min_abv = float(min_abv) if arg_exists(min_abv) else None
+	max_abv = request.args.get('maxabv')
+	max_abv = float(max_abv) if arg_exists(max_abv) else None
 	drink_name = request.args.get('drink') # for searching for similar drinks
 
 	if drink_name:
 		print("User searched for drinks similar to {}".format(drink_name))
-		results = search_drinks(drink_name, k=10)
-
+		results = search_drinks(
+			data=drink_name,
+			k=10,
+			page=page_number,
+			pmin=min_price,
+			pmax=max_price,
+			amin=min_abv,
+			amax=max_abv,
+			base=base
+		)
+		for i in range(len(results)):
+			results[i][1] = json.loads(results[i][0].reviews) if results[i][0].reviews is not None else []
+		
 		if results is not None:
 			return render_template('results.html', results=results, page_number=page_number, drink_name=drink_name)
 	
@@ -44,11 +63,21 @@ def search():
 		desc_lst = [d.strip().lower().replace(' ', '_') for d in descriptors.split(',')]
 		print("User searched for a {} with descriptors: {}".format(drink_type, descriptors))
 
-		results = search_drinks(desc_lst, dtype=None if drink_type == 'anything' else drink_type, k=10)
-		for i in range(len(results)):
-			results[i][1] = json.loads(results[i][0].reviews) if results[i][0].reviews is not None else []
+		results = search_drinks(
+			data=desc_lst,
+			dtype=None if drink_type == 'anything' else drink_type,
+			k=10,
+			page=page_number,
+			pmin=min_price,
+			pmax=max_price,
+			amin=min_abv,
+			amax=max_abv,
+			base=base
+		)
 
 		if results is not None:
+			for i in range(len(results)):
+				results[i][1] = json.loads(results[i][0].reviews) if results[i][0].reviews is not None else []
 			return render_template('results.html', results=results, page_number=page_number, drink_type=drink_type, base=base, descriptors=descriptors, min_price=min_price, max_price=max_price)
 
 	descriptors = [e.word.replace('_', ' ') for e in query_embeddings()]
