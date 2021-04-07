0219de2b841e6880fa37f3b86c18a480efde4791
Kendall Lane <kal255@cornell.edu>
2021-04-07 10:48:57 -0400

    Fix Column Names and Clean Up combine_datasets.py

--- combine_datasets.py
+++ combine_datasets.py
@@ -1,20 +1,15 @@
 import pandas as pd
   
-# reading two csv files
 data1 = pd.read_csv('datasets/All_Streaming_Shows.csv')
 data2 = pd.read_csv('datasets/tv_shows.csv')
   
-# using merge function by setting how='inner'
 output = pd.merge(data1, data2, 
                    on='Title', 
                    how='left')
 
-# remove columns
+# remove and rename columns
 output = output.loc[:, ~output.columns.duplicated()]
 output = output.drop(columns=['ID', 'Netflix',	'Hulu', 'Prime Video', 'Disney+', 'type', 'Year_y', 'Content Rating_y'])
+output = output.rename(columns={"Year_x": "Year", "Content Rating_x": "Content Rating"})
 
-# displaying result
-print(output)
-
-#output into file
-output.to_csv('final_data.csv')
+output.to_csv('kaggle_data.csv')
