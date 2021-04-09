07ba173a9a395851dcfccaf9e271440ca5cf7631
oz28 <oz28@cornell.edu>
2021-04-09 16:32:21 +0000

    random recipes

--- test.py
+++ test.py
@@ -1,15 +1,18 @@
 from recipe_scrapers import scrape_me
 import pandas as pd
+import random
 
-
+authors = []
 titles = []
 total_times = []
 yields = []
 ingredients = []
+instructions = []
 images = []
 host = []
 links = []
 nutrients = []
+ratings = []
 
 # scraper = scrape_me(
 #     "https://www.allrecipes.com/recipe/" + str() + "/")
@@ -17,24 +20,29 @@ nutrients = []
 # random sample
 # do we include things with empty fields?
 count = 0
-i = 15868
 while count < 100:
+    i = random.randint(0, 300000)
+
     print(i)
     # try:
     scraper = scrape_me(
         "https://www.allrecipes.com/recipe/" + str(i) + "/")
     title = scraper.title()
     if title == "":
-        i += 1
+        # i += 1
         continue
+    print("found")
     titles.append(title)
+    authors.append(scraper.author())
     total_times.append(scraper.total_time())
     yields.append(scraper.yields())
     ingredients.append(scraper.ingredients())
+    instructions.append(scraper.instructions())
     images.append(scraper.instructions())
     host.append(scraper.host())
     nutrients.append(scraper.nutrients())
-    i += 1
+    ratings.append(scraper.ratings())
+    # i += 1
     count += 1
     # except:
     # i += 1
@@ -42,6 +50,7 @@ while count < 100:
     # print(titles)
 
 print(titles)
+#remove duplicates?
 
 recipes = pd.DataFrame({
     'title': titles,
