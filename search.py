171f86ce5cae1a3dd4409dd2674d5ccc88873912
sn532-4300 <81885372+sn532-4300@users.noreply.github.com>
2021-04-13 16:00:28 -0400

    search strategy

--- search.py
+++ search.py
@@ -4,7 +4,8 @@ Searches for recipes similar to movie and returns top ten.
 import sim
  
 """
-Step 1: generate similarity matrix
+Given a query (movie name), a dict of movies to food words, and a list of recipes
+Step 1: generate or access similarity matrix
 Step 2: use movie_name_to_index to retrieve row corresponding to query, or return error if movie not found
 Step 3: use index_to_recipe to create list of tuples (similarity score, recipe), sort by similarity score,
 return recipes in sorted order
