d9f3798a027b46df05834cd3a787f76564137eb0
mcb273 <mcb273@cornell.edu>
2021-04-13 19:34:08 -0400

    edit project title on app

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -2,7 +2,7 @@ from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
 
-project_name = "City Recommendations"
+project_name = "Ski Resort Recommendations"
 net_id = "Ava Anderson: aca76, Michael Behrens: mcb273, Cameron Haarmann: cmh332, Nicholas Mohan: nhm39, Megan Tormey: mt664"
 
 
