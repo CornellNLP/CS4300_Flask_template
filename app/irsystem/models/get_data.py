b5fd3980b84e08318e29dcf48b0716c77067983e
Ryan Richardson <35542359+ryan26192@users.noreply.github.com>
2021-04-15 19:35:18 -0400

    tokens per trail and trail_to_idx dict in get_data

--- app/irsystem/models/get_data.py
+++ app/irsystem/models/get_data.py
@@ -4,4 +4,11 @@ import json
 with open('../../../scrapers/ithacatrails.json') as f:
     data = json.load(f)
 
+def get_trail_to_idx():
+    trail_to_id = {}
+    for i, trail in enumerate(data):
+        trail_to_id[trail] = i
+    return trail_to_id
+
+trail_to_idx = get_trail_to_idx()
 # pprint.pprint(data['Ellis Hollow Yellow trail'])
--- app/irsystem/models/tokens.py
+++ app/irsystem/models/tokens.py
@@ -1,4 +1,4 @@
-from get_data import data 
+from get_data import data, trail_to_idx
 import re
 from nltk.tokenize import TreebankWordTokenizer
 
@@ -12,6 +12,7 @@ class Tokens:
     """
 
     tokens = []
+    tokens_per_trail = {}
     tokens_to_idx = None
     
     def __init__(self, token_type='reviews'):
@@ -19,47 +20,81 @@ class Tokens:
                               'reviews', 
                               'attributes', 
                               'descriptions']
-        # print(token_type)
         if token_type == 'reviews and descriptions':
-            self.tokens = self._get_tokens(descriptions = True)
+            self.tokens_per_trail = self._get_tokens_per_trail(descriptions = True)
         elif token_type == 'reviews':
-            self.tokens = self._get_tokens()
+            self.tokens_per_trail = self._get_tokens_per_trail()
         elif token_type == 'descriptions':
-            self.tokens = self._get_tokens(reviews = False, descriptions = True)
+            self.tokens_per_trail = self._get_tokens_per_trail(reviews = False, descriptions = True)
         else:
-            self.tokens = self._get_tokens_attributes()
+            self.tokens_per_trail = self._get_tokens_per_trail_attributes()
+        self.tokens = self._get_tokens()
         self.tokens_to_idx = self._build_tokens_to_idx()
-        
-    def _get_tokens(self, reviews = True, descriptions = False):
-        tokens = set()
-        # tokens2 = set()
+    
+    def _get_tokens_per_trail(self, reviews = True, descriptions = False):
+        tokens_per_trail = {}
         tokenize = TreebankWordTokenizer().tokenize
-        for trail in data:
+        for i, trail in enumerate(data):
+            tokens = set()
             if reviews:
                 for review in data[trail]['Reviews']:
                     tokens.update(tokenize(review['comment'].lower()))
-                    # tokens2.update(re.findall('[a-z]+', review['comment'].lower()))
             if descriptions:
-                # tokens2.update(re.findall('[a-z]+', data[trail]['Description'].lower()))
                 tokens.update(tokenize(data[trail]['Description'].lower()))
+            tokens_per_trail[i] = list(tokens)
+        return tokens_per_trail
+    
+    def _get_tokens_per_trail_attributes(self):
+        tokens_per_trail = {}
+        for i, trail in enumerate(data):
+            tokens_per_trail[i] = list(set(data[trail]['Trail Attributes']))
+        return tokens_per_trail
+        # for i, trail in enumerate(data):
+
+        # tokens = set()
+        # for trail in data:
+        #     tokens.update(data[trail]['Trail Attributes'])
+        # return list(tokens)
+
+    def _get_tokens(self, reviews = True, descriptions = False):
+        # tokens = set()
+        # # tokens2 = set()
+        # tokenize = TreebankWordTokenizer().tokenize
+        # for trail in data:
+        #     if reviews:
+        #         for review in data[trail]['Reviews']:
+        #             tokens.update(tokenize(review['comment'].lower()))
+        #             # tokens2.update(re.findall('[a-z]+', review['comment'].lower()))
+        #     if descriptions:
+        #         # tokens2.update(re.findall('[a-z]+', data[trail]['Description'].lower()))
+        #         tokens.update(tokenize(data[trail]['Description'].lower()))
+        # return list(tokens)
+        tokens = set()
+        for trail in self.tokens_per_trail:
+            tokens_of_trail = self.tokens_per_trail[trail]
+            tokens.update(tokens_of_trail)
         return list(tokens)
 
-    def _get_tokens_attributes(self):
-        tokens = set()
-        for trail in data:
-            tokens.update(data[trail]['Trail Attributes'])
-        return list(tokens)
+    # def _get_tokens_attributes(self):
+    #     tokens = set()
+    #     for trail in data:
+    #         tokens.update(data[trail]['Trail Attributes'])
+    #     return list(tokens)
 
     def _build_tokens_to_idx(self):
         tokens_to_index = {}
         for i in range(len(self.tokens)):
-            print(self.tokens[i])
             tokens_to_index[self.tokens[i]] = i
-        print(tokens_to_index)
         return tokens_to_index
 
 ## TEST CODE
-# token = Tokens(token_type ='attributes')
-# tokens = token.tokens
+tokens_obj = Tokens(token_type='attributes')
+tokens_per_trail = tokens_obj.tokens_per_trail
+# print(trail_to_idx)
+# print(token._get_tokens_per_trail()[1])
+# print(tokens_per_trail[trail_to_idx['Ellis Hollow Red trail']] == tokens_per_trail[1])
+# print(tokens_per_trail[1])
+# print(token.tokens)
+# print(len(token.tokens))
 # print(token.tokens_to_idx['Restrooms available'])
 
