eccc3bd418efd107853acbec1863b81867e54762
oz28 <oz28@cornell.edu>
2021-04-16 01:10:35 +0000

    reorg files

--- sim.py
+++ sim.py
@@ -23,6 +23,7 @@ import numpy as np
 
 def tokenize(text):
     """Returns a list of words that make up the text.    
+    Borrowed code from Assignment 4
     Params: {text: String}
     Returns: Array
     """
@@ -36,49 +37,6 @@ def tokenize_titles(recipe_list):
     for recipe in recipe_list:
         res.append(tokenize(recipe))
     return res
-
-
-def build_inverted_index(titles):
-    """ Builds an inverted index from the messages.
-
-    Arguments
-    =========
-
-    titles: tokenized recipe titles
-
-    Returns
-    =======
-
-    inverted_index: dict
-        For each term, the index contains 
-        a sorted list of tuples (doc_id, count_of_term_in_doc)
-        such that tuples with smaller doc_ids appear first:
-        inverted_index[term] = [(d1, tf1), (d2, tf2), ...]
-
-    Example
-    =======
-
-    >> test_idx = build_inverted_index([
-    ...    {'toks': ['to', 'be', 'or', 'not', 'to', 'be']},
-    ...    {'toks': ['do', 'be', 'do', 'be', 'do']}])
-
-    >> test_idx['be']
-    [(0, 2), (1, 2)]
-
-    >> test_idx['not']
-    [(0, 1)]
-
-    Note that doc_id refers to the index of the document/message in msgs.
-    """
-    # YOUR CODE HERE
-    res = defaultdict(list)
-    for i in range(len(titles)):
-        words = titles[i]
-        word_dict = {}
-        for word in set(words):
-            res[word] += [(i, words.count(word))]
-    return res
-
 
 def build_vectorizer(recipe_list):
     """Returns a TfidfVectorizer object with certain preprocessing properties.
@@ -115,7 +73,8 @@ def get_movie_tfidfs(movie_food_words, vectorizer):
 
 def get_cos_sim(recipe_mat, movie_mat):
     """Returns the cosine similarity of two movie scripts.
-
+    Modified code from Assignment 5
+    
     Params: {mov1: String,
              mov2: String,
              input_doc_mat: np.ndarray,
