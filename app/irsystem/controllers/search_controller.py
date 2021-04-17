f4781fcf3b456a2ad2b1620ae40530467a5e8f8d
oz28 <oz28@cornell.edu>
2021-04-17 21:40:51 +0000

    updates to controller

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,51 +1,62 @@
-from . import *  
+from . import *
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
-import scripts.sim 
+import json
+import pandas as pd
+import scripts.sim as sim
 from scripts.search import run_search
 project_name = "Screen to Table"
 net_id = "Olivia Zhu(oz28), Daniel Ye(dzy3), Shivank Nayak(sn532), Kassie Wang(klw242), Elizabeth Healy(eah255)"
 
-#toy dataset
-recipe_list = ["Double Cheeseburger", "Cheeseburger Sliders", "Pop-Tarts",
-								"Blueberry Pancakes", "Shrimp and Catfish Gumbo", "Cajun Shrimp", "Shrimp Burgers"]
-movie_list = {"Pulp Fiction": [
-				"burger, cheeseburger"], "Forrest Gump": ["shrimp", "chocolates"]}
+# toy dataset
+# recipe_list = ["Double Cheeseburger", "Cheeseburger Sliders", "Pop-Tarts",
+# 								"Blueberry Pancakes", "Shrimp and Catfish Gumbo", "Cajun Shrimp", "Shrimp Burgers"]
+# movie_list = {"Pulp Fiction": [
+# 				"burger, cheeseburger"], "Forrest Gump": ["shrimp", "chocolates"]}
 
-@irsystem.route('/')
-def home():
-	query = request.args.get('search')
-	if not query:
-		data = []
-		output_message = ''
-		return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
-	else:
-		output_message = "Your search: " + query
-		data = run_search(recipe_list, movie_list, query)
-		return redirect(url_for('irsystem.get_results', data=data))
+with open('movie_food_words_from_wordnets.json') as f:
+    movie_list = json.loads(f)
+with open('data/clean_recipes.csv') as f:
+    recipes = pd.DataFrame(f)
+
+sim_mat = sim.get_cos_sim
+
 
 @irsystem.route('/', methods=['GET'])
-def search():
-	query = request.args.get('search')
-	if not query:
-		data = []
-		output_message = ''
-	else:
-		output_message = "Your search: " + query
-		data = run_search(recipe_list, movie_list, query)
-		# data = mat_search(query, sim_mat, movie_to_index, recipe_list)
-	# return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
-	return redirect(url_for('irsystem.get_results', data=data))
+def home():
+    query = request.args.get('search')
+    if not query:
+        data = []
+        output_message = ''
+        return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
+    else:
+        output_message = "Your search: " + query
+        data = run_search(sim_mat, movie_list, query)
+        return redirect(url_for('irsystem.get_results', data=data))
+
+
+# @irsystem.route('/', methods=['GET'])
+# def search():
+#     query = request.args.get('search')
+#     if not query:
+#         data = []
+#         output_message = ''
+#     else:
+#         output_message = "Your search: " + query
+#         data = run_search(sim_mat, movie_list, query)
+#         # data = mat_search(query, sim_mat, movie_to_index, recipe_list)
+#     # return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
+#     return redirect(url_for('irsystem.get_results', data=data))
+
 
 @irsystem.route('/results/<data>')
 def get_results(data):
-	return render_template('results.html')
+    return render_template('results.html')
+
 
 @irsystem.route('/recipe/<title>')
 def get_recipe(title):
-	title = title
-	ingredients = ["apples", "oranges"]
-	steps = ["cook", "clean"]
-	return render_template('recipe.html', ingredients=ingredients, steps=steps)
-
-
+    title = title
+    ingredients = ["apples", "oranges"]
+    steps = ["cook", "clean"]
+    return render_template('recipe.html', ingredients=ingredients, steps=steps)
--- scripts/search.py
+++ scripts/search.py
@@ -2,7 +2,7 @@
 Searches for recipes similar to movie and returns top ten. 
 """
 import scripts.sim as sim
- 
+
 """
 Given a query (movie name), a dict of movies to food words, and a list of recipes
 Step 1: generate or access similarity matrix
@@ -11,12 +11,14 @@ Step 3: use index_to_recipe to create list of tuples (similarity score, recipe),
 return recipes in sorted order
 """
 
+
 def movie_to_index_maker(m_dict):
     m_to_i = {}
     m_list = [m for m in m_dict.keys()]
     for i in range(len(m_list)):
         m_to_i[m_list[i]] = i
     return m_to_i
+
 
 def mat_search(query, sim_mat, movie_to_index, recipe_list):
     if query not in movie_to_index:
@@ -29,23 +31,25 @@ def mat_search(query, sim_mat, movie_to_index, recipe_list):
     results = [(r[1], r[0]) for r in sorted(recipe_tuples, reverse=True)]
     return results[:10]
 
-def run_search(recipe_list, movie_list, query):
-    recipe_mat, vectorizer = sim.build_vectorizer(recipe_list)
-    movie_mat = sim.get_movie_tfidfs(movie_list, vectorizer)
-    sim_mat = sim.get_cos_sim(recipe_mat, movie_mat)
+
+def run_search(sim_mat, movie_list, query):
+    # recipe_mat, vectorizer = sim.build_vectorizer(recipe_list)
+    # movie_mat = sim.get_movie_tfidfs(movie_list, vectorizer)
+    # sim_mat = sim.get_cos_sim(recipe_mat, movie_mat)
 
     movie_to_index = movie_to_index_maker(movie_list)
 
     res = mat_search(query, sim_mat, movie_to_index, recipe_list)
 
-    print(res) 
+    print(res)
     return res
+
 
 if __name__ == "__main__":
     recipe_list = ["Double Cheeseburger", "Cheeseburger Sliders", "Pop-Tarts",
-                    "Blueberry Pancakes", "Shrimp and Catfish Gumbo", "Cajun Shrimp", "Shrimp Burgers"]
+                   "Blueberry Pancakes", "Shrimp and Catfish Gumbo", "Cajun Shrimp", "Shrimp Burgers"]
     movie_list = {"Pulp Fiction": [
-            "burger, cheeseburger"], "Forrest Gump": ["shrimp", "chocolates"]}
+        "burger, cheeseburger"], "Forrest Gump": ["shrimp", "chocolates"]}
     query = "Forrest Gump"
 
     run_search(recipe_list, movie_list, query)
