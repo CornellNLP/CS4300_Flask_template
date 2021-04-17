8c73e76c0879a8abcf750aeed4006f1cd21b78bd
Liam Daniels <whd56@cornell.edu>
2021-04-16 23:38:47 -0400

    Ingredient query, meat aliases

--- app/irsystem/ingredients/ingredients.py
+++ app/irsystem/ingredients/ingredients.py
@@ -4,9 +4,12 @@ within the recipe dataset, including
 tokenization and similarity measures.
 
 Author: Liam Daniels
-Date: 15 April 2021
+Date: 16 April 2021
 """
 import pandas as pd
+import numpy as np
+
+ING_CATEGORY_NAME = "ingredients" 
 
 def tokenize_recipe_ingredients(df):
     """
@@ -20,7 +23,6 @@ def tokenize_recipe_ingredients(df):
     # want to isolate words like "beef" in ingredients
     # like "ground beef."
     DELIM = " "
-    ING_CATEGORY_NAME = "ingredients" 
     STRIP = "[] ',"
 
     ing_str_as_list = lambda ing: [s.strip(STRIP) for s in ing.split(DELIM)]
@@ -28,3 +30,79 @@ def tokenize_recipe_ingredients(df):
 
     return df
 
+def calc_edit_distance(s1, s2):
+    """ Calculates Levenshtein edit distance between two strings"""
+    INSERT_COST = 1
+    DELETE_COST = 1
+    SUB_COST    = 2
+    NO_COST     = 0
+    sub_func = lambda q, d, i, j: NO_COST if q[i - 1] == d[j - 1] else SUB_COST
+
+    m = len(s1) + 1
+    n = len(s2) + 1
+
+    matrix = np.zeros((m, n))
+    for i in range(1, m):
+            matrix[i, 0] = matrix[i - 1, 0] + DELETE_COST
+    for j in range(1, n):
+        matrix[0, j] = matrix[0, j - 1] + INSERT_COST
+    for i in range(1, m):
+        for j in range(1, n):
+            matrix[i, j] = min(
+                matrix[i - 1, j] + DELETE_COST,
+                matrix[i, j - 1] + INSERT_COST, 
+                matrix[i - 1, j - 1] + sub_func(s1, s2, i, j) 
+            )
+
+    return matrix[-1][-1]
+
+def contains_ingredient(recipe_series, ingredient_query, max_dist=2):
+    """
+    Checks whether or not a given recipe has an ingredient that
+    is within a certain edit distance from the inputted ingredient 
+    string. 
+
+    Precondition: ingredients have already been tokenized.
+
+    Parameters:
+    -----------------
+    recipe_series : pandas.Series
+        A row in the recipes dataframe, representing one recipe.
+
+    ingredient_query : str
+        The ingredient being checked for within the recipe's ingredients.
+
+    max_dist : int
+        The largest edit distance such that two strings are still 
+        considered equal.
+
+    Returns:
+    ------------------
+    bool
+        Whether recipe's ingredients contains a token within max_dist of
+        the inputted ingredient.
+    """
+    ingredients = recipe_series[ING_CATEGORY_NAME]
+    for ing in ingredients:
+        if calc_edit_distance(ing, ingredient_query) <= max_dist:
+            return True
+    return False
+
+def make_meat_alias_dict(ALIAS_CSV="meat_aliases.csv"):
+    """
+    Creates dictionary that maps obscure meat parts to their
+    common names. Based on specific file structure of a CSV
+    that must be in the directory with a specific name.
+    """
+    with open(ALIAS_CSV) as f:
+        lines = f.readlines()
+    lines = [l.split(",") for l in lines[1:]]
+
+    aliases = {}
+    for l in lines:
+        # Element 0 is original name, all others are aliases
+        for i in range(1, len(l)):
+            alias = l[i].strip()
+            original = l[0].strip()
+            aliases[alias] = original
+    return aliases
--- app/irsystem/ingredients/test.py
+++ app/irsystem/ingredients/test.py
@@ -23,8 +23,61 @@ def test_tokenize():
         test_eq(name, output, expected)
     for t in tests: test_by_loc(t[0], t[1], t[2], t[3])
 
+def test_edit_distance():
+    tests = [
+        ("Egg and eggs", "egg", "eggs", 1),
+        ("Eggs and eggz", "eggs", "eggz", 2),
+        ("Eggs and egg", "eggs", "egg", 1),
+        ("Kardashian dalmatian", "kardashian", "dalmatian", 9)
+    ]
+
+    for t in tests: 
+        output = ingr.calc_edit_distance(t[1], t[2])
+        expected = t[3]
+        name = t[0]
+        test_eq(name, output, expected)
+
+def test_contains_ingredient():
+    tests = [
+        ("First rec contains water", 0, "water", 2, True),
+        ("First rec doesn't contain beef", 0, "beef", 2, False),
+        ("First rec contains dates", 0, "dates", 2, True),
+        ("First rec contains date", 0, "date", 2, True),
+        ("First rec contains dat", 0, "dat", 2, True),
+        ("First rec doesn't contains tate", 0, "tate", 2, False)
+    ]
+
+    df = ingr.tokenize_recipe_ingredients(pd.read_csv(RECIPE_FILE))
+    for t in tests:
+        name = t[0]
+        series = df.loc[t[1]]
+        query = t[2]
+        max_dist = t[3]
+        expected = t[4]
+        output = ingr.contains_ingredient(series, query, max_dist)
+        test_eq(name, output, expected)
+        
+def test_meat_aliases():
+    tests = [
+        ("chuck is beef", "chuck", "beef"),
+        ("loin is beef", "loin", "beef"),
+        ("hock is pork", "hock", "pork")
+    ]
+
+    meat = ingr.make_meat_alias_dict()
+    for t in tests:
+        output = meat[t[1]]
+        name = t[0]
+        expected = t[2]
+        test_eq(name, output, expected)
+
+
+
 def test_all():
     test_tokenize()
+    test_edit_distance()
+    test_contains_ingredient()
+    test_meat_aliases()
     print("All tests pass!")
 
 test_all()
