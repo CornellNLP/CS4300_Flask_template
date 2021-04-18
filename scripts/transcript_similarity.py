7214c3455bc6ece579cc77ba53b11c2b22083578
Siddhi Chordia <sc2538@cornell.edu>
2021-04-18 19:52:42 -0400

    listTranscripts

--- scripts/transcript_similarity.py
+++ scripts/transcript_similarity.py
@@ -1,9 +1,9 @@
 import re
-
+import os
 
 def readTranscript (filePath):
     """
-    given a file path, return the relevant part of the transcript file
+    given a string filePath, return the relevant part of the transcript file
     """
     file = open(filePath)
     fileContents = file.read()
@@ -14,11 +14,30 @@ def readTranscript (filePath):
 #transcript = readTranscript("/Users/siddhichordia/cs4300sp2020-rj356-dd492-sc2538-sv352-kal255-1/transcripts/Avatar: The Last Airbender/avatar_scripts_s1_e2.txt")
 
 def tokenize (transcript):
+    """
+    given a string transcript, return a list of tokens
+    """
     text = transcript.lower()
     regex = r'[a-z]+'
     #print("words:" , re.findall(regex,text))
     return re.findall(regex,text)
 
 #tokenize(transcript)
+
+def listTranscripts(showFolder):
+    """
+    given a string showFolder (path to show folder), return a list of the transcript files for that show
+    """
+    result = []
+    for sub, dirs, transcripts in os.walk(showFolder):
+        for file in transcripts:
+            filepath = sub + os.sep + file
+            result.append(filepath)
+    print(result)
+    return (result)
+    
+listTranscripts("/Users/siddhichordia/cs4300sp2020-rj356-dd492-sc2538-sv352-kal255-1/transcripts/American Crime Story")
+
+
 
 
