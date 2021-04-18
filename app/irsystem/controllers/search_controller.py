4257e83d48e4d4c6bbf5ec707e2c15ff2361589f
Doreen Gui <dg497@cornell.edu>
2021-04-18 14:36:09 -0400

    modifed search result

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,20 +1,25 @@
 from . import *  
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
+import json
 
 project_name = "Ilan's Cool Project Template"
 net_id = "Ilan Filonenko: if56"
 
 @irsystem.route('/', methods=['GET'])
 def search():
-	query = request.args.get('search')
-	if not query:
-		data = []
-		output_message = ''
-	else:
-		output_message = "Your search: " + query
-		data = range(5)
-	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
+  query = request.args.get('search')
+  if not query:
+    data = []
+    output_message = ''
+  else:
+    output_message = "Your search: " + query
+
+    # Todo: get data from scripts
+    with open('app/static/exampleOutput.json', encoding='utf-8') as f:
+      data = json.load(f)
+
+  return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
 
 
 
