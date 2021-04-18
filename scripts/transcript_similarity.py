eabb88b78a39cab0eb7836ee0576aa728898860d
Siddhi Chordia <sc2538@cornell.edu>
2021-04-18 18:24:20 -0400

    debugging readTranscript

--- scripts/transcript_similarity.py
+++ scripts/transcript_similarity.py
@@ -2,5 +2,7 @@ def readTranscript (filePath):
     file = open(filePath)
     fileContents = file.read()
     file.close()
+    print(fileContents)
     return fileContents
 
+readTranscript("/Users/siddhichordia/cs4300sp2020-rj356-dd492-sc2538-sv352-kal255-1/transcripts/Avatar: The Last Airbender/avatar_scripts_s1_e2.txt")
