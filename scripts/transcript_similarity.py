81ad49d18b5edf45131e9f3d9ce186b1e52b6cdd
Siddhi Chordia <sc2538@cornell.edu>
2021-04-18 18:50:28 -0400

    tokenize function

--- scripts/transcript_similarity.py
+++ scripts/transcript_similarity.py
@@ -1,3 +1,6 @@
+import re
+
+
 def readTranscript (filePath):
     """
     given a file path, return the relevant part of the transcript file
@@ -5,7 +8,15 @@ def readTranscript (filePath):
     file = open(filePath)
     fileContents = file.read()
     file.close()
-    print(fileContents)
+    #print(fileContents)
     return fileContents
 
-readTranscript("/Users/siddhichordia/cs4300sp2020-rj356-dd492-sc2538-sv352-kal255-1/transcripts/Avatar: The Last Airbender/avatar_scripts_s1_e2.txt")
+transcript = readTranscript("/Users/siddhichordia/cs4300sp2020-rj356-dd492-sc2538-sv352-kal255-1/transcripts/Avatar: The Last Airbender/avatar_scripts_s1_e2.txt")
+
+def tokenize (transcript):
+    text = transcript.lower()
+    regex = r'[a-z]+'
+    #print("words:" , re.findall(regex,text))
+    return re.findall(regex,text)
+
+tokenize(transcript)
