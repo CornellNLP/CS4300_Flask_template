11702996819e8dc09531ba78f51a5660542dd928
Junho Kim-Lee <junhokimlee@gmail.com>
2021-04-18 02:08:03 -0400

    Initial changes- will update commit msg later

--- app/irsystem/controllers/json_reader.py
+++ app/irsystem/controllers/json_reader.py
@@ -2,6 +2,7 @@
 
 import json
 import pandas as pd
+import numpy as np
 
 
 def json_read(file_name):
@@ -13,3 +14,23 @@ def json_read(file_name):
         data = json.load(f)
     df = pd.DataFrame(data)
     return df
+
+
+def json_read_vector(file_name):
+    """
+    Takes a file_path for a json file of type wine_variety_vectors.json and
+    returns a legend where legend[i] is the personality trait being described
+    at position i, index where index[i] is the wine variety being described at
+    position i, and a matrix where matrix[i][j] is the magnitude of personality
+    type j in wine i.
+    """
+    with open(file_name) as f:
+        data = json.load(f)
+    legend = data["legend"]
+    vectors = data["data"]
+    wine_personality_matrix = np.zeros((len(vectors), len(legend)))
+    index = []
+    for count, i in enumerate(vectors):
+        index.append(i["variety"])
+        wine_personality_matrix[count] = i["personality_vector"]
+    return legend, index, wine_personality_matrix
--- app/irsystem/controllers/main.py
+++ app/irsystem/controllers/main.py
@@ -1,43 +1,52 @@
 # Module to call in the terminal to run the IR system.
 
-from json_reader import json_read
+from json_reader import *
 from cosine_similarity import *
+from personality_vector import *
 
 print("Welcome to Winetime!")
 print("Loading data...")
 df = json_read("winemag_data_withtoks.json")
 df_personality = json_read("wine_personality.json")
+legend, index, mat = json_read_vector("wine_variety_vectors.json")
 print("Precomputing resources...")
 inv_ind, idf, norms = precompute(df["toks"])
 
-# Personality data 
+# Personality data
 tokenized_personality = tokenizer_personality_data(df_personality)
 tokenized_variety = tokenizer_personality_variety(df_personality)
 flat_tokenized_variety = flat_tokenizer_personality_variety(df_personality)
-inv_ind_person, idf_person, norms_person = precompute_personality(tokenized_personality)
+inv_ind_person, idf_person, norms_person = precompute_personality(
+    tokenized_personality)
 
 print("Load successful!")
 print()
 
 quit = False
 while not quit:
-    name = input("What is your name? ")
-    personality = input("How would your friends describe your personality? ")
-    flavor = input("Describe your favorite drink (can be non-alcoholic!) ")
-    scent = input("Describe your favorite scent ")
-    #q = input("Enter your wine keywords (\"quit\" to exit): ")
+    print("What is your name?")
+    name = input("> ")
+    # personality=input("How would your friends describe your personality? ")
+    personality = generate_personality_vec()
+    print("Describe your favorite drink (can be non-alcoholic!).")
+    flavor = input("> ")
+    print("Describe your favorite scent.")
+    scent = input("> ")
+    # q = input("Enter your wine keywords (\"quit\" to exit): ")
     # if name == "quit" or personality == "quit" or flavor == "quit" or scent == "quit":
     #     quit = True
     #     break
     flavor_result = cossim_dict(flavor, inv_ind, idf, norms)
     scent_result = cossim_dict(scent, inv_ind, idf, norms)
-    personality_result = cossim_dict(personality, inv_ind_person, idf_person, norms_person)
-    personality_result_nondict = cossim(personality, inv_ind_person, idf_person, norms_person)
-    total = total_score(flavor_result, scent_result, personality_result, df, tokenized_variety, flat_tokenized_variety)
-    #a = cossim(q, inv_ind, idf, norms)
+    # personality_result=cossim_dict(
+    #    personality, inv_ind_person, idf_person, norms_person)
+    # personality_result_nondict=cossim(
+    #    personality, inv_ind_person, idf_person, norms_person)
+    total = total_score(flavor_result, scent_result)
+    # a = cossim(q, inv_ind, idf, norms)
     # print(a)
-    #display(q, a, df, 10)
+    # display(q, a, df, 10)
     display(name, total, df, 10)
-    display_personality(name,personality_result_nondict, df_personality)
+    #display_personality(name, personality_result_nondict, df_personality)
 
     print()
