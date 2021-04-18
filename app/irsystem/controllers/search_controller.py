2788a7ea2dce905959da096271099eea5c856c26
aliciaxw <alicia.xinran.wang@gmail.com>
2021-04-18 16:17:37 -0400

    Update search template

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,6 +1,7 @@
 from . import *  
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
+from app.irsystem.models.result import Result
 
 project_name = "Hiking Trail Recommender"
 net_id = "Ryan Richardson: rrr225" + \
@@ -11,12 +12,17 @@ net_id = "Ryan Richardson: rrr225" + \
 @irsystem.route('/', methods=['GET'])
 def search():
 	query = request.args.get('search')
+
+	# Retrieve rankings
+	test_result = Result(name="Cascadilla Gorge Trail", length=3.5)
+	results = [test_result]
+
 	if not query:
 		data = []
 		output_message = ''
 	else:
 		output_message = "Your search: " + query
-		data = range(5)
+		data = results
 	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
 
 
