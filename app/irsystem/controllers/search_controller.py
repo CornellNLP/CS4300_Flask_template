96d357620a33c91800209e49585a286c90a74562
oz28 <oz28@cornell.edu>
2021-04-21 01:31:10 +0000

    updates to routes

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -9,82 +9,50 @@ from scripts.search import run_search
 project_name = "Screen to Table"
 net_id = "Olivia Zhu(oz28), Daniel Ye(dzy3), Shivank Nayak(sn532), Kassie Wang(klw242), Elizabeth Healy(eah255)"
 
-# toy dataset
-# recipe_list = ["Double Cheeseburger", "Cheeseburger Sliders", "Pop-Tarts",
-# 								"Blueberry Pancakes", "Shrimp and Catfish Gumbo", "Cajun Shrimp", "Shrimp Burgers"]
-# movie_list = {"Pulp Fiction": [
-# 				"burger, cheeseburger"], "Forrest Gump": ["shrimp", "chocolates"]}
-
-with open('./data/movie_food_words_from_wordnets_top2.json') as f:
-    movie_list = json.load(f)
-with open('./data/recipe_data/clean_recipes.csv') as f:
-    csvreader = csv.DictReader(f, delimiter=';')
-    recipes = []
-    for row in csvreader:
-        recipes.append(row)
-with open('./data/movie_recipe_mat_top2.csv') as f:
-    csvreader = csv.reader(f, delimiter=',')
-    movie_recipe_mat = []
-    for row in csvreader:
-        movie_recipe_mat.append(row)
-# recipes = pd.read_csv('./data/recipe_data/clean_recipes.csv', sep=';')
-# movie_recipe_mat = pd.read_csv('./data/movie_recipe_mat.csv')
-# print(recipes)
-# print(movie_recipe_mat)
-
 
 @irsystem.route('/', methods=['GET'])
 def home():
     query = request.args.get('search')
+    msg = request.args.get('msg')
     if not query:
         data = []
         output_message = ''
         return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
     else:
-        output_message = "Your search: " + query
-        res = run_search(movie_recipe_mat, movie_list, query, recipes)
-        if res == None:
-            output_message = res
-            return render_template('search.html', name=project_name, netid=net_id, output_message=output_message)
-        else:
-            data = json.dumps(res)
-            return redirect(url_for('irsystem.get_results', data=data))
+        return redirect(url_for('irsystem.get_results', query=query))
 
 
-# @irsystem.route('/', methods=['GET'])
-# def search():
-#     query = request.args.get('search')
-#     if not query:
-#         data = []
-#         output_message = ''
-#     else:
-#         output_message = "Your search: " + query
-#         data = run_search(sim_mat, movie_list, query)
-#         # data = mat_search(query, sim_mat, movie_to_index, recipe_list)
-#     # return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
-#     return redirect(url_for('irsystem.get_results', data=data))
+@irsystem.route('/results')
+def get_results():
+    query = request.args.get("query")
+    data = run_search(query)
+
+    if data == None:
+        return redirect(url_for('irsystem.home', msg="try again"))
+        return "No results :("
+    else:
+        res = []
+
+        for d in data:
+            idx = int(d[0])
+            r = recipes[idx]
+            res.append((idx, r))
+        return render_template('results.html', res=res)
 
 
-@irsystem.route('/results/<data>')
-def get_results(data):
-    res = []
-    # print(data)
-    data = json.loads(data)
-    for d in data:
-        idx = int(d[0])
-        r = recipes[idx]
-        res.append((idx, r))
-    return render_template('results.html', res=res)
-
-
-@irsystem.route('/recipe/<idx>')
-def get_recipe(idx):
-    idx = int(idx)
+@irsystem.route('/recipe')
+def get_recipe():
+    idx = int(request.args.get("idx"))
     recipe = recipes[idx]
     title = recipe['Recipe Name']
     ingredients = recipe['Ingredients']
     steps = recipe['Directions']
+    author = recipe['Author']
     # title = 'Recipe Name'
     # ingredients = 'Ingredients'
     # steps = 'Directions'
-    return render_template('recipe.html', title=title, ingredients=ingredients, steps=steps)
+    return render_template('recipe.html',
+                           title=title,
+                           ingredients=ingredients,
+                           steps=steps,
+                           author=author)
--- scripts/search.py
+++ scripts/search.py
@@ -14,6 +14,20 @@ Step 3: use index_to_recipe to create list of tuples (similarity score, recipe),
 return recipes in sorted order
 """
 
+# load data
+with open('./data/movie_food_words_from_wordnets_top2.json') as f:
+    movie_list = json.load(f)
+with open('./data/recipe_data/clean_recipes.csv') as f:
+    csvreader = csv.DictReader(f, delimiter=';')
+    recipes = []
+    for row in csvreader:
+        recipes.append(row)
+with open('./data/movie_recipe_mat_top2.csv') as f:
+    csvreader = csv.reader(f, delimiter=',')
+    movie_recipe_mat = []
+    for row in csvreader:
+        movie_recipe_mat.append(row)
+
 
 def movie_to_index_maker(m_dict):
     m_to_i = {}
@@ -23,12 +37,14 @@ def movie_to_index_maker(m_dict):
     return m_to_i
 
 
+movie_to_index = movie_to_index_maker(movie_list)
+
+
 def mat_search(query, sim_mat, movie_to_index, recipe_list):
 
-    if query not in movie_to_index:
+    if not validate_query(query):
         return None
     query_index = movie_to_index[query]
-    # print(query_index)
     recipe_scores = sim_mat[query_index]
     recipe_tuples = []
     for i in range(1, len(recipe_list)+1):
@@ -37,9 +53,12 @@ def mat_search(query, sim_mat, movie_to_index, recipe_list):
     return results[:10]
 
 
-def run_search(sim_mat, movie_list, query, recipes):
-    movie_to_index = movie_to_index_maker(movie_list)
-    res = mat_search(query, sim_mat, movie_to_index, recipes)
+def validate_query(query):
+    return query in movie_to_index
+
+
+def run_search(query):
+    res = mat_search(query, movie_recipe_mat, movie_to_index, recipes)
 
     return res
 
