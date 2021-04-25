54e423274d2f90e9c85d2af403a264b66f1ad0a1
Doreen Gui <dg497@cornell.edu>
2021-04-25 19:33:51 -0400

    plot markers on map

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -163,7 +163,6 @@ def search():
     else:
         output_message = "Your search was point of interest: " + query_int + ", location: " + query_loc + ", radius: " + query_rad
         exists = True
-        # Todo: get data from scripts
         data = get_covid_data(query_loc, query_int, query_rad, 2.0)
 
     return render_template('new-search-page.html', name=project_name, netid=net_id, output_message=output_message, data=data, exists=exists)
